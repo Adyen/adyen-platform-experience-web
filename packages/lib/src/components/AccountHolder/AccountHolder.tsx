898e2bb2d33e8285cef115d374a29fddd344d1b0
pablo <pablo.igarzabal@adyen.com>
2023-02-14 15:26:09 +0100

    Add Account holder details component

---
+++ packages/lib/src/components/AccountHolder/AccountHolder.tsx
@@ -0,0 +1,45 @@
+import { h } from 'preact';
+import CoreProvider from '../../core/Context/CoreProvider';
+import UIElement from '../UIElement';
+import AccountHolderDetails from './components/AccountHolderDetails';
+
+export class BalanceAccountElement extends UIElement {
+    public static type = 'transactions';
+
+    get isValid() {
+        return !!this.state.isValid;
+    }
+
+    get displayName() {
+        return this.props.name;
+    }
+
+    formatProps(props) {
+        return props;
+    }
+
+    /**
+     * Formats the component data output
+     */
+    formatData() {
+        return {
+            ...this.state,
+        };
+    }
+
+    render() {
+        return (
+            <CoreProvider i18n={this.props.i18n} loadingContext={this.props.loadingContext}>
+                <AccountHolderDetails
+                    ref={ref => {
+                        this.componentRef = ref;
+                    }}
+                    {...this.props}
+                    onChange={this.setState}
+                />
+            </CoreProvider>
+        );
+    }
+}
+
+export default BalanceAccountElement;
