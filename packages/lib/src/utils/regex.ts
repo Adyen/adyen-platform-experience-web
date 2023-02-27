1bc0c6a1f11d85c374cf310352fed7e9a6892486
pablo <pablo.igarzabal@adyen.com>
2023-02-27 17:43:18 +0100

    Add missing utils

---
+++ packages/lib/src/utils/regex.ts
@@ -0,0 +1,9 @@
+/**
+ * Email regex
+ */
+export const email = /^\s*[\w\-+_]+(\.[\w\-+_]+)*@[\w\-+_]+\.[\w\-+_]+(\.[\w-+_]+)*\s*$/;
+
+/**
+ * Telephone number regex
+ */
+export const telephoneNumber = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s./0-9]*$/;
