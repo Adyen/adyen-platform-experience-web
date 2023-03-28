9e248b9dca3558bcb9dc387cae6caf7778c2af67
germanm <german.mora@adyen.com>
2023-03-28 10:19:13 +0200

    Fix Language utils type errors

--- packages/lib/src/utils/types.ts
+++ packages/lib/src/utils/types.ts
@@ -1,4 +1,1 @@
 export type ValueOf<T> = T[keyof T];
-export function isNotUndefinedArray<T>(a_arr: Array<T | undefined>): a_arr is Array<T> {
-    return !a_arr.some(a_item => a_item === undefined);
-}
