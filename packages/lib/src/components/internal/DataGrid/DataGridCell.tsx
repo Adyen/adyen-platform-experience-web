9a9917b9cbfc8a5f09920ea6cc98731ee448699c
germanm <german.mora@adyen.com>
2023-03-31 10:51:23 +0200

    Fixed types on internal components - Data Grid

--- packages/lib/src/components/internal/DataGrid/DataGridCell.tsx
+++ packages/lib/src/components/internal/DataGrid/DataGridCell.tsx
@@ -1,4 +1,6 @@
-export default function DataGridCell({ children, ...props }) {
+import { HTMLAttributes, PropsWithChildren } from 'preact/compat';
+
+export default function DataGridCell({ children, ...props }: PropsWithChildren<HTMLAttributes<HTMLTableCellElement>>) {
     return (
         <td class="adyen-fp-data-grid__cell" {...props}>
             {children}
