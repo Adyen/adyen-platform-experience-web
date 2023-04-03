8a63356ba808f4b3a7033bbb53b4bc07ad04d1fe
germanm <german.mora@adyen.com>
2023-04-03 10:47:04 +0200

    Fixed types on internal components - Modal

--- packages/lib/src/components/internal/Modal/Modal.tsx
+++ packages/lib/src/components/internal/Modal/Modal.tsx
@@ -2,17 +2,24 @@ import { useEffect, useRef } from 'preact/hooks';
 import cx from 'classnames';
 import './Modal.scss';
 import { ModalProps } from './types';
+import { TargetedEvent } from 'preact/compat';
 
+function targetIsNode(e: EventTarget | null): e is Node {
+    if (!e || !('nodeType' in e)) {
+        return false;
+    }
+    return true;
+}
 export default function Modal({ title, children, classNameModifiers = [], isOpen, onClose, isDismissible = true }: ModalProps) {
     const modalContainerRef = useRef<HTMLDivElement>(null);
 
-    const handleClickOutside = e => {
-        if (isDismissible && isOpen && !modalContainerRef?.current?.contains(e.target)) {
+    const handleClickOutside = (e: TargetedEvent<Node, MouseEvent>) => {
+        if (isDismissible && isOpen && targetIsNode(e.target) && !modalContainerRef?.current?.contains(e.target)) {
             onClose();
         }
     };
 
-    const handleEscKey = e => {
+    const handleEscKey = (e: KeyboardEvent) => {
         if (e.key === 'Escape' && isOpen && isDismissible) {
             onClose();
         }
@@ -44,8 +51,8 @@ export default function Modal({ title, children, classNameModifiers = [], isOpen
                         <button onClick={onClose} className={`adyen-fp-modal__header-icon`}>
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
-                                    fill-rule="evenodd"
-                                    clip-rule="evenodd"
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
                                     d="M13.6569 4.07112C14.0474 3.6806 14.0474 3.04743 13.6569 2.65691C13.2664 2.26638 12.6332 2.26638 12.2427 2.65691L8.00005 6.89954L3.75745 2.65694C3.36692 2.26641 2.73376 2.26641 2.34324 2.65694C1.95271 3.04746 1.95271 3.68063 2.34324 4.07115L6.58584 8.31375L2.34319 12.5564C1.95266 12.9469 1.95266 13.5801 2.34319 13.9706C2.73371 14.3611 3.36688 14.3611 3.7574 13.9706L8.00005 9.72796L12.2427 13.9706C12.6333 14.3612 13.2664 14.3612 13.6569 13.9706C14.0475 13.5801 14.0475 12.947 13.6569 12.5564L9.41426 8.31375L13.6569 4.07112Z"
                                     fill="#687282"
                                 />
