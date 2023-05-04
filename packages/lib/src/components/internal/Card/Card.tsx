b8dcfc7248e5d2308aaac030c740384c25181a0c
germanm <german.mora@adyen.com>
2023-05-04 12:02:50 +0200

    Fix collapsible component

--- packages/lib/src/components/internal/Card/Card.tsx
+++ packages/lib/src/components/internal/Card/Card.tsx
@@ -2,10 +2,9 @@ import { CardProps } from './types';
 import { PropsWithChildren } from 'preact/compat';
 import './Card.scss';
 import classNames from 'classnames';
-import { useState } from 'preact/hooks';
+import { useCallback, useState } from 'preact/hooks';
 import CollapsibleContainer from '../CollapsibleContainer/CollapsibleContainer';
 const Card = ({
-    onClickHandler,
     title,
     subTitle,
     children,
@@ -18,10 +17,7 @@ const Card = ({
     openByDefault = true,
 }: PropsWithChildren<CardProps>) => {
     const [isOpen, setIsOpen] = useState(openByDefault);
-    const toggleIsOpen = () => {
-        // onClickHandler?.();
-        setIsOpen(!isOpen);
-    };
+    const toggleIsOpen = useCallback(() => setIsOpen(isOpen => !isOpen), []);
     return (
         <section className={classNames('adyen-fp-card', { 'adyen-fp-card--filled': filled, 'adyen-fp-card--no-outline': noOutline })}>
             <header className={classNames('adyen-fp-card__header', { 'adyen-fp-card__header-collapsible': collapsible })}>
@@ -31,7 +27,7 @@ const Card = ({
                             className="adyen-fp-card__collapse-button"
                             aria-expanded={`${!!isOpen}`}
                             aria-controls="collapsible-container-id"
-                            aria-label="useful-label-text"
+                            aria-label="tooggle-content"
                             onClick={toggleIsOpen}
                         >
                             <span
@@ -46,13 +42,7 @@ const Card = ({
                 )}
                 {(title || renderHeader) && (
                     <div className="adyen-fp-card__header-collapsible-content">
-                        {renderHeader ? (
-                            renderHeader
-                        ) : (
-                            <span className="adyen-fp-card__title" onClick={onClickHandler}>
-                                {title}
-                            </span>
-                        )}
+                        {renderHeader ? renderHeader : <span className="adyen-fp-card__title">{title}</span>}
                         {subTitle && <div className="adyen-fp-card__subtitle">{subTitle}</div>}
                     </div>
                 )}
--- packages/lib/src/components/internal/CollapsibleContainer/CollapsibleContainer.tsx
+++ packages/lib/src/components/internal/CollapsibleContainer/CollapsibleContainer.tsx
@@ -1,29 +1,22 @@
 import classNames from 'classnames';
 import { PropsWithChildren } from 'preact/compat';
 import './CollapsibleContainer.scss';
-import { useChildrenHeight } from './useChildrenHeight';
 import { useEffect, useState } from 'preact/hooks';
 
 function CollapsibleContainer({ isOpen, children }: PropsWithChildren<{ isOpen: boolean }>) {
-    const [height, childrenRef] = useChildrenHeight();
     const [withAnimation, setWithAnimation] = useState(false);
     useEffect(() => {
         if (!withAnimation && !isOpen) setWithAnimation(true);
-    }, [isOpen]);
+    }, [isOpen, withAnimation]);
 
     return (
         <div
             className={classNames('adyen-fp-collapsible-container', {
-                'adyen-fp-collapsible-container--with-transition': !isOpen || (isOpen && withAnimation),
+                'adyen-fp-collapsible-container--with-transition': !isOpen || withAnimation,
                 'adyen-fp-collapsible-container--collapsed': !isOpen,
             })}
-            style={{
-                height: isOpen ? height : 0,
-            }}
         >
-            <div className="adyen-fp-collapsible-container__content" ref={childrenRef}>
-                {children}
-            </div>
+            <div className="adyen-fp-collapsible-container__content">{children}</div>
         </div>
     );
 }
