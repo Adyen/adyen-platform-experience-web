9e86069e612ca995904caeb7df1fd84c784bccf7
germanm <german.mora@adyen.com>
2023-05-05 10:13:47 +0200

    Creating structuredList component

---
+++ packages/lib/src/components/internal/StructuredList/useStructuredListItems.ts
@@ -0,0 +1,21 @@
+import uuidv4 from '../../../utils/uuid';
+import { StructuredListItem, ListValue } from './types';
+import { TranslationKey } from '../../../language/types';
+import useCoreContext from '../../../core/Context/useCoreContext';
+export const useStructuredListItems = (items: { [key in TranslationKey]?: ListValue | undefined }) => {
+    const { i18n } = useCoreContext();
+    return Object.keys(items).reduce((prev: StructuredListItem[], key) => {
+        const uniqueID = uuidv4();
+        const itemKey = key as TranslationKey;
+        return items[itemKey]
+            ? [
+                  ...prev,
+                  {
+                      id: uniqueID,
+                      label: i18n.get(itemKey),
+                      value: items[itemKey],
+                  },
+              ]
+            : prev;
+    }, []);
+};
