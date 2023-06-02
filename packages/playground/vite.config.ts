c0ff59c6fbb8936f2cea9375a82dc50322660dfe
germanm <german.mora@adyen.com>
2023-06-02 17:08:47 +0200

    Separate vite config according to the different packages

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite';
 import { preact } from '@preact/preset-vite';
 import { resolve } from 'node:path';
 import { lstat, readdir } from 'node:fs/promises';
-import { mockApiProxies, mockServerPlugin } from './packages/server/proxy/mockApiProxies';
-import { getEnvironment } from './envs/getEnvs';
-import { realApiProxies } from './packages/server/proxy/realApiProxies';
+import { mockApiProxies, mockServerPlugin } from '../server/proxy/mockApiProxies';
+import { getEnvironment } from '../../envs/getEnvs';
+import { realApiProxies } from '../server/proxy/realApiProxies';
 import { checker } from 'vite-plugin-checker';
-import version from './packages/lib/config/version';
-import packageJson from './packages/lib/package.json';
 
-const currentVersion = version();
-
-const playgroundDir = resolve(__dirname, 'packages/playground/src/pages');
-const demoPlaygroundDir = resolve(__dirname, 'packages/playground/');
+const playgroundDir = resolve(__dirname, 'src/pages');
+const demoPlaygroundDir = resolve(__dirname, './');
 async function getPlaygroundEntrypoints() {
     const playgroundPages = await readdir(playgroundDir);
 
@@ -29,8 +25,6 @@ async function getPlaygroundEntrypoints() {
         index: resolve(playgroundDir, '..', '..', 'index.html'),
     };
 }
-
-const externalDependencies = Object.keys(packageJson.dependencies);
 
 export default defineConfig(async ({ mode }) => {
     const { lemApi, BTLApi, BCLApi, playground, mockServer } = getEnvironment(mode);
@@ -57,22 +51,11 @@ export default defineConfig(async ({ mode }) => {
                       emptyOutDir: true,
                       target: 'esnext',
                   }
-                : {
-                      lib: {
-                          name: 'AdyenFPComponents',
-                          entry: resolve(__dirname, './packages/lib/src/index.ts'),
-                          formats: ['cjs', 'es'],
-                          fileName: format => `adyen-fp-components.${format}.js`,
-                      },
-                      minify: true,
-                      rollupOptions: { external: externalDependencies },
-                      outDir: resolve(__dirname, 'packages', 'lib', 'dist'),
-                      emptyOutDir: false,
-                  },
+                : undefined,
         resolve: {
             alias: {
-                '@adyen/adyen-fp-web': resolve(__dirname, 'packages/lib/src'),
-                '@src': resolve(__dirname, 'packages/lib/src'),
+                '@adyen/adyen-fp-web': resolve(__dirname, '../lib/src'),
+                '@src': resolve(__dirname, '../lib/src'),
             },
         },
         css: {
@@ -99,10 +82,6 @@ export default defineConfig(async ({ mode }) => {
         },
         define: {
             'process.env.VITE_BALANCE_PLATFORM': JSON.stringify(BTLApi.balancePlatform || null),
-            'process.env.VITE_VERSION': JSON.stringify(currentVersion.ADYEN_FP_VERSION),
-            'process.env.VITE_COMMIT_HASH': JSON.stringify(currentVersion.COMMIT_HASH),
-            'process.env.VITE_COMMIT_BRANCH': JSON.stringify(currentVersion.COMMIT_BRANCH),
-            'process.env.VITE_ADYEN_BUILD_ID': JSON.stringify(currentVersion.ADYEN_BUILD_ID),
         },
     };
 });
