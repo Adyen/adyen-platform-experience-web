c6bb2e309fe6421f8ae6ab0c0fcd807aef354a4b
germanm <german.mora@adyen.com>
2023-06-27 14:51:01 +0200

    Use normal imports in legalEntityDetails

--- packages/lib/src/components/LegalEntityDetails/components/LegalEntityDetails/LegalEntityDetails.tsx
+++ packages/lib/src/components/LegalEntityDetails/components/LegalEntityDetails/LegalEntityDetails.tsx
@@ -1,27 +1,22 @@
 import './LegalEntityDetails.scss';
 import { LegalEntityDetailsProps } from '../../types';
 import Card from '../../../internal/Card/Card';
-import LoadingWrapper from '@src/components/internal/LoadingWrapper';
-import { Suspense, lazy } from 'preact/compat';
-
-const LegalEntityIndividual = lazy(() => import('../LegalEntityIndividual/LegalEntityIndividual'));
-const LegalEntityOrganization = lazy(() => import('../LegalEntityOrganization/LegalEntityOrganization'));
-const LegalEntitySoleProprietor = lazy(() => import('../LegalEntitySoleProprietor/LegalEntitySoleProprietor'));
+import LegalEntityIndividual from '../LegalEntityIndividual/LegalEntityIndividual';
+import LegalEntityOrganization from '../LegalEntityOrganization/LegalEntityOrganization';
+import LegalEntitySoleProprietor from '../LegalEntitySoleProprietor/LegalEntitySoleProprietor';
 
 const LegalEntityDetails = ({ legalEntity, onGetTransferInstrument }: LegalEntityDetailsProps) => {
     return (
         <div className="adyen-legal-entity">
-            <Suspense fallback={<LoadingWrapper />}>
-                <Card>
-                    {legalEntity?.type === 'individual' ? (
-                        <LegalEntityIndividual legalEntity={legalEntity} />
-                    ) : legalEntity?.type === 'organization' ? (
-                        <LegalEntityOrganization legalEntity={legalEntity} onGetTransferInstrument={onGetTransferInstrument} />
-                    ) : legalEntity?.type === 'soleProprietorship' ? (
-                        <LegalEntitySoleProprietor legalEntity={legalEntity} />
-                    ) : null}
-                </Card>
-            </Suspense>
+            <Card>
+                {legalEntity?.type === 'individual' ? (
+                    <LegalEntityIndividual legalEntity={legalEntity} />
+                ) : legalEntity?.type === 'organization' ? (
+                    <LegalEntityOrganization legalEntity={legalEntity} onGetTransferInstrument={onGetTransferInstrument} />
+                ) : legalEntity?.type === 'soleProprietorship' ? (
+                    <LegalEntitySoleProprietor legalEntity={legalEntity} />
+                ) : null}
+            </Card>
         </div>
     );
 };
