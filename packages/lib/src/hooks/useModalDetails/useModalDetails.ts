5292cff5e64f4424ca15172cae08498c260e0c2a
germanm <german.mora@adyen.com>
2023-10-16 14:00:57 +0200

    Move types of showDetailsModal hook to a separate file

--- packages/lib/src/hooks/useModalDetails/useModalDetails.ts
+++ packages/lib/src/hooks/useModalDetails/useModalDetails.ts
@@ -1,40 +1,5 @@
 import { useCallback, useMemo, useState } from 'preact/hooks';
-import { TranslationKey } from '@src/core/Localization/types';
-import { ModalSize } from '@src/components/internal/Modal/types';
-
-type SelectedDetail<Options> = {
-    title: TranslationKey;
-    selection: { type: keyof Options; detail: any };
-    modalSize?: ModalSize;
-};
-
-type CallbackFunction<T> = (arg: T) => void;
-
-type DetailsConfig<T> = {
-    showDetails: (boolean | undefined)[];
-    callback?: CallbackFunction<T>;
-};
-
-type GetArgsExceptCallback<T extends Required<DetailsConfig<any>>> = Omit<Parameters<T['callback']>[0], 'showModal'>;
-
-type ModalDetailsOptions = {
-    [k: string]: DetailsConfig<any>;
-};
-
-type CallbackIsPresent<Options extends ModalDetailsOptions, T extends SelectedDetail<Options>> = Options[T['selection']['type']] extends {
-    callback: any;
-}
-    ? true
-    : false;
-
-type CallbackParams<Options extends ModalDetailsOptions, T extends SelectedDetail<Options>> = {
-    callback: (
-        args: Options[T['selection']['type']] extends { callback: any } ? GetArgsExceptCallback<Required<Options[T['selection']['type']]>> : never
-    ) => void;
-};
-function hasCallback(options: any): options is Required<DetailsConfig<any>> {
-    return 'callback' in options;
-}
+import { CallbackIsPresent, CallbackParams, GetArgsExceptCallback, hasCallback, ModalDetailsOptions, SelectedDetail } from './types';
 
 /**
  * @param options - This parameter is an object that contains several fields.
