d9a3494b467378877568b43641fd9138b8e62e26
glad <glad.chinda@adyen.com>
2023-11-02 15:09:59 +0100

    feat: added useDefinedValue hook

---
+++ packages/lib/src/hooks/useDefinedValue/useDefinedValue.ts
@@ -0,0 +1,17 @@
+import { useMemo, useRef } from 'preact/hooks';
+import { isFunction } from '@src/utils/common';
+
+const useDefinedValue = <T = any>(fallback: T | (() => T), value?: T | null) => {
+    const rawValueRef = useRef(value ?? null);
+    const fallbackFactory = useMemo(() => (isFunction(fallback) ? fallback : () => fallback), [fallback]);
+
+    const definedValue = useMemo(() => {
+        const currentValue = value ?? null;
+        if (rawValueRef.current !== currentValue) rawValueRef.current = currentValue;
+        return rawValueRef.current;
+    }, [value]);
+
+    return useMemo(() => definedValue ?? fallbackFactory(), [definedValue]);
+};
+
+export default useDefinedValue;
