b6a694262d7e1cfe43bf8b6b45cfc145393eb102
germanm <german.mora@adyen.com>
2023-11-16 17:24:59 +0100

    Add preferredTranslations to list of excluded props in localization

--- packages/lib/src/core/Localization/constants/localization.ts
+++ packages/lib/src/core/Localization/constants/localization.ts
@@ -31,4 +31,4 @@ export const SUPPORTED_LOCALES = [
 
 export const DEFAULT_DATETIME_FORMAT = { year: 'numeric', month: '2-digit', day: '2-digit' } as Intl.DateTimeFormatOptions;
 export const DEFAULT_LOCALES = SUPPORTED_LOCALES as Readonly<SupportedLocale[]>;
-export const EXCLUDE_PROPS = ['constructor', 'i18n', 'watch'] as const;
+export const EXCLUDE_PROPS = ['constructor', 'i18n', 'watch', 'preferredTranslations'] as const;
