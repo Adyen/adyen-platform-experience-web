5667da42d5d0c725053f5993c209bf4b1b027b1d
glad <glad.chinda@adyen.com>
2023-12-15 12:37:45 +0100

    Fixed reflexes
    - Updated reflex core logic for reliable and consistent behavior
    - Added unit tests for reflex core logic
    - Updated existing reflex-based hooks
    - Modified the `enumerable` util function to allow optional `writable` argument
    - Added `hasOwnProperty` util function

--- packages/lib/src/components/internal/FilterBar/components/FilterButton/FilterButton.tsx
+++ packages/lib/src/components/internal/FilterBar/components/FilterButton/FilterButton.tsx
@@ -3,8 +3,7 @@ import { TypographyElement, TypographyVariant } from '@src/components/internal/T
 import Typography from '@src/components/internal/Typography/Typography';
 import { parseClassName } from '@src/utils/class-name-utils';
 import { parseBoolean } from '@src/utils/common';
-import { Ref } from 'preact';
-import { forwardRef } from 'preact/compat';
+import { ForwardedRef, forwardRef } from 'preact/compat';
 import { useMemo } from 'preact/hooks';
 import { FilterButtonProps } from './types';
 import './FilterButton.scss';
@@ -13,7 +12,7 @@ const DEFAULT_FILTER_BUTTON_CLASSNAME = 'adyen-fp-filter-button';
 
 function FilterButton(
     { className, classNameModifiers = [], children, disabled, onClick, ...restAttributes }: FilterButtonProps,
-    ref: Ref<HTMLButtonElement>
+    ref: ForwardedRef<HTMLButtonElement>
 ) {
     const classNameValue = useMemo(() => parseClassName('', className) || '', [className]);
     const disabledValue = useMemo(() => parseBoolean(disabled), [disabled]);
