90fc4debeeaffc2ffb2896b163facaed886d347c
germanm <german.mora@adyen.com>
2023-12-20 12:08:00 +0100

    Fix position and styles

--- packages/lib/src/components/internal/Tooltip/TooltipContent.tsx
+++ packages/lib/src/components/internal/Tooltip/TooltipContent.tsx
@@ -8,43 +8,56 @@ interface TooltipContentProps {
     onMouseLeave: any;
     content: string;
 }
+
+const ARROW_HEIGHT = 5;
 export const TooltipContent = forwardRef<HTMLDivElement, TooltipContentProps>(({ isVisible, controllerRef, onMouseLeave, content }, tooltipRef) => {
-    const controllerPosition = useMemo(() => controllerRef.current?.getBoundingClientRect(), [controllerRef.current]);
+    const [tooltip, setTooltip] = useState({ width: 0, height: 0 });
+
     const contentRef = useReflex(
         useCallback(() => {
-            console.log(controllerRef.current.clientHeight);
             if (isVisible) setTooltip({ width: contentRef?.current?.clientWidth ?? 0, height: contentRef?.current?.clientHeight ?? 0 });
         }, [isVisible]),
         tooltipRef as Reflexable<HTMLDivElement>
     );
 
-    const [tooltip, setTooltip] = useState({ width: 0, height: 0 });
     const tooltipPosition = useMemo(() => {
-        if (controllerPosition) {
+        const controllerRect = controllerRef.current?.getBoundingClientRect();
+
+        if (controllerRect) {
             return {
-                top: controllerPosition.top - (controllerRef.current?.clientHeight ?? 0) - tooltip.height - 40,
-                left: controllerPosition.left + window.scrollX + controllerPosition.width / 2 - tooltip.width / 2,
+                top: controllerRect.top - controllerRect.height - tooltip.height / 2 - ARROW_HEIGHT,
+                left: controllerRect.left - 3 + window.scrollX + controllerRect.width / 2 - tooltip.width / 2,
             };
         }
         return {
             top: 0,
             left: 0,
         };
-    }, [controllerRef.current, tooltip.width]);
+    }, [controllerRef.current, tooltip.width, tooltip.height]);
 
     return (
         <div
-            style={`transform: translate3d(${tooltipPosition.left}px, ${tooltipPosition.top}px, 0px)`}
-            className={cx('adyen-fp-tooltip__content', {
-                'adyen-fp-tooltip__content--hidden': tooltip.width === 0,
+            className={cx('adyen-fp-tooltip__container', {
+                'adyen-fp-tooltip__container--hidden': tooltip.width === 0,
             })}
-            id="tooltip"
-            role="tooltip"
-            aria-hidden={!isVisible}
-            ref={contentRef}
-            onMouseLeave={onMouseLeave}
         >
-            {content}
+            <div
+                style={`transform: translate3d(${tooltipPosition.left}px, ${tooltipPosition.top}px, 0px)`}
+                className="adyen-fp-tooltip__content"
+                id="tooltip"
+                role="tooltip"
+                aria-hidden={!isVisible}
+                ref={contentRef}
+                onMouseLeave={onMouseLeave}
+            >
+                {content}
+            </div>
+            <span
+                className="adyen-fp-tooltip__arrow"
+                style={`transform: translate3d(${tooltipPosition.left + tooltip.width / 2}px, ${
+                    tooltipPosition.top + tooltip.height - ARROW_HEIGHT
+                }px, 0px) rotate(45deg)`}
+            ></span>
         </div>
     );
 });
