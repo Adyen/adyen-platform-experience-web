32973328294a8027d8dc356c294d92ca71e04345
germanm <german.mora@adyen.com>
2024-01-16 10:33:01 +0100

    use DEPLOY_PRIME_URL

--- packages/playground/.storybook/utils/create-adyenFP.ts
+++ packages/playground/.storybook/utils/create-adyenFP.ts
@@ -2,7 +2,7 @@ import { CoreOptions } from '@adyen/adyen-fp-web';
 import { AdyenFP } from '@adyen/adyen-fp-web';
 
 export const createAdyenFP = async (context: CoreOptions) => {
-    console.log('context1', context);
+    console.log('context2', context);
     const adyenSession = await AdyenFP(context);
     return adyenSession;
 };
