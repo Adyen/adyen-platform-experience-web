4d184784097e136830f731702ce7473917cb72ab
germanm <german.mora@adyen.com>
2024-01-18 09:27:06 +0100

    Create hook for interactive Data Grid

--- packages/lib/src/components/internal/DataGrid/DataGrid.tsx
+++ packages/lib/src/components/internal/DataGrid/DataGrid.tsx
@@ -3,9 +3,8 @@ import classnames from 'classnames';
 import Spinner from '../Spinner';
 import DataGridCell from './DataGridCell';
 import './DataGrid.scss';
-import { useCallback, useMemo, useReducer } from 'preact/hooks';
-import useReflex from '@src/hooks/useReflex';
-import { InteractionKeyCode } from '@src/components/types';
+import { useCallback, useMemo } from 'preact/hooks';
+import { useInteractiveDataGrid } from '@src/components/internal/DataGrid/hooks/useInteractiveDataGrid';
 
 export enum CellTextPosition {
     CENTER = 'center',
@@ -95,115 +94,20 @@ function DataGridBody<Items extends Array<any>, ClickedField extends keyof Items
     );
     const handleOnClick = useMemo(() => (props.onRowClick ? onClickCallBack : undefined), [onClickCallBack, props.onRowClick]);
 
-    const [state, dispatch] = useReducer<Readonly<{ activeIndex: number; index: number }>, { type: 'NEXT'; index?: number; activeIndex?: number }>(
-        (currentState, action) => {
-            switch (action.type) {
-                case 'NEXT': {
-                    const total = props.data.length;
-
-                    if (total > 1) {
-                        const current = currentState.index;
-                        const nextIndex = action.index;
-
-                        if (nextIndex === undefined) return currentState;
-
-                        if ((nextIndex < total && nextIndex! >= 0) || (current === 0 && nextIndex > 0)) {
-                            return Object.freeze({ ...currentState, index: action.index ?? 0, activeIndex: action.activeIndex ?? -1 });
-                        }
-                    }
-                    return currentState;
-                }
-                default:
-                    return currentState;
-            }
-        },
-        INITIAL_STATE
-    );
-
-    const cursor = useReflex<Element>(
-        useCallback(
-            current => {
-                if (!(current instanceof Element)) return;
-
-                const optionIndex = Number((current as HTMLElement).dataset?.index);
-
-                if ((state.activeIndex === -1 && optionIndex === 0) || optionIndex === state.index) {
-                    current.setAttribute('tabindex', '0');
-                } else {
-                    current.setAttribute('tabindex', '-1');
-                }
-                if (optionIndex === state.activeIndex) {
-                    (current as HTMLElement)?.focus();
-                }
-            },
-            [state.activeIndex, state.index]
-        )
-    );
-
-    const onKeyDownCapture = useCallback(
-        (evt: KeyboardEvent) => {
-            const isRow = (evt.target as HTMLElement)?.localName === 'tr';
-            if (!isRow) {
-                if (evt.code === InteractionKeyCode.ARROW_LEFT) {
-                    dispatch({
-                        type: 'NEXT',
-                        index: state.index,
-                        activeIndex: state.index,
-                    });
-                }
-                return;
-            }
-            switch (evt.code) {
-                case InteractionKeyCode.ARROW_DOWN:
-                case InteractionKeyCode.ARROW_UP:
-                    dispatch({
-                        type: 'NEXT',
-                        index: evt.code === InteractionKeyCode.ARROW_DOWN ? state.index + 1 : state.index - 1,
-                        activeIndex: evt.code === InteractionKeyCode.ARROW_DOWN ? state.index + 1 : state.index - 1,
-                    });
-                    break;
-                case InteractionKeyCode.HOME:
-                    dispatch({
-                        type: 'NEXT',
-                        index: 0,
-                        activeIndex: 0,
-                    });
-                    break;
-                case InteractionKeyCode.END:
-                    dispatch({
-                        type: 'NEXT',
-                        index: props.data.length - 1,
-                        activeIndex: props.data.length - 1,
-                    });
-                    break;
-                case InteractionKeyCode.ENTER:
-                    (evt.currentTarget as HTMLElement)?.click();
-                    break;
-                default:
-                    return;
-            }
-            evt.stopPropagation();
-        },
-
-        [props.data.length, state.index]
-    );
+    const { currentIndex, ref, listeners } = useInteractiveDataGrid({ totalRows: props.data.length });
 
     return (
         <tbody className="adyen-fp-data-grid__body">
             {props.data.map((item, index) => (
                 <tr
-                    onFocusCapture={evt => {
-                        if (state.index === -1) dispatch({ type: 'NEXT', index: index });
-                        const isRow = (evt.target as HTMLElement)?.localName === 'tr';
-                        if (!isRow) dispatch({ type: 'NEXT', index: index });
-                    }}
-                    onKeyDownCapture={onKeyDownCapture}
-                    ref={cursor}
-                    aria-selected={index === state.index}
+                    ref={ref}
+                    aria-selected={index === currentIndex}
                     data-index={index}
                     className={classNames}
                     key={item}
                     onClick={handleOnClick?.(item)}
+                    onFocusCapture={listeners.onFocusCapture(index)}
+                    onKeyDownCapture={listeners.onKeyDownCapture}
                 >
                     {props.columns.map(({ key }) => {
                         if (props.customCells?.[key])
