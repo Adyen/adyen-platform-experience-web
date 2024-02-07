c413ce4d75f068d333c6456f1b2061a536ae4936
germanm <german.mora@adyen.com>
2024-02-07 12:34:47 +0100

    Memoize imageUrl in Image component

--- packages/lib/src/components/internal/Image/Image.tsx
+++ packages/lib/src/components/internal/Image/Image.tsx
@@ -1,5 +1,6 @@
 import cx from 'classnames';
 import { useImageUrl } from '@src/hooks/useImageUrl/useImageUrl';
+import { useMemo } from 'preact/hooks';
 
 interface ImageProps {
     name: string;
@@ -10,10 +11,15 @@ interface ImageProps {
 }
 
 export const Image = ({ folder = 'components/', className, alt, name, ...props }: ImageProps) => {
-    const imageUrl = useImageUrl({
-        options: { imageFolder: folder, ...props },
-        name: name,
-    });
+    const imageUrl = useImageUrl(
+        useMemo(
+            () => ({
+                options: { imageFolder: folder, ...props },
+                name,
+            }),
+            [folder, name, props]
+        )
+    );
 
     return <img className={cx('adyen-fp__image', className)} alt={alt} src={imageUrl} />;
 };
