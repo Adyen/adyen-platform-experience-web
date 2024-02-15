a23bcf56413bc0844e57b80428ed2af5c27c1b97
germanm <german.mora@adyen.com>
2024-02-15 12:20:41 +0100

    Fix e2e test

--- packages/playground/src/endpoints/utils/utils.ts
+++ packages/playground/src/endpoints/utils/utils.ts
@@ -1,10 +1,10 @@
 import { compose, context, DelayMode } from 'msw';
 
-const IS_TEST = process.env.E2E_TEST || process.env.VITE_MODE === 'demo';
+const IS_TEST = Boolean(process.env.E2E_TEST === 'true') || process.env.VITE_MODE === 'demo';
 export function delay(duration?: DelayMode | number): any {
     // Ensure there is no response delay in tests.
 
-    if (IS_TEST === 'true') {
+    if (IS_TEST) {
         return compose();
     }
 
