23321e45e7b38759fef3fe6705203d3bc2f078f6
isuert <isuert.mjalti@adyen.com>
2024-02-23 16:16:57 +0100

    Introduce expandable card

---
+++ packages/lib/src/components/internal/ExpandableCard/constants.ts
@@ -0,0 +1,8 @@
+export const BASE_CLASS = 'adyen-fp-expandable-card';
+export const CONTAINER_CLASS = BASE_CLASS + '__container';
+export const CONTAINER_FILLED_CLASS = CONTAINER_CLASS + '--filled';
+export const CONTAINER_HIDDEN_CLASS = CONTAINER_CLASS + '--hidden';
+export const CONTAINER_OVERLAY_CLASS = CONTAINER_CLASS + '--overlay';
+export const CONTAINER_OVERLAY_ID = CONTAINER_OVERLAY_CLASS;
+export const CONTENT_CLASS = BASE_CLASS + '__content';
+export const CHEVRON_CLASS = BASE_CLASS + '__chevron';
