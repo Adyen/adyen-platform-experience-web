c6756a30846fdb3e0a7b541bc434724f88f79dc4
glad <glad.chinda@adyen.com>
2024-02-28 20:46:53 +0100

    Fix cursor pagination to use new next/previous pattern

--- packages/lib/src/components/external/Transactions/components/utils.ts
+++ packages/lib/src/components/external/Transactions/components/utils.ts
@@ -1,4 +1,3 @@
-import { PageNeighbour, PaginatedResponseDataWithLinks } from '../../../internal/Pagination/types';
 import { ITransaction } from '../../../../types';
 
 const labels = {
@@ -20,35 +19,9 @@ const labels = {
     paymentMethod: 'paymentMethod',
     currency: 'currency',
 } as const;
+
 export const getLabel = (key: keyof typeof labels) => {
     return labels[key] || key;
-};
-
-export const getCursor = (dir: PageNeighbour, transactions: PaginatedResponseDataWithLinks<ITransaction, 'data'>): string | null => {
-    try {
-        if (transactions._links?.[dir]?.href) {
-            const url = new URL(transactions?._links?.[dir]?.href ?? '');
-            return url.searchParams.get('cursor');
-        }
-    } catch (e) {
-        console.error(e);
-    }
-    return null;
-};
-
-export const getRequestParams = (transactions: PaginatedResponseDataWithLinks<ITransaction, 'data'>) => {
-    try {
-        const links = transactions?._links || {};
-        const link = links['prev'] || links['next'];
-        if (link?.href) {
-            const url = new URL(link.href);
-            const { cursor, ...params } = Object.fromEntries(url.searchParams);
-            return params;
-        }
-    } catch (e) {
-        console.error(e);
-    }
-    return null;
 };
 
 const PAYMENT_METHODS = Object.freeze({
