12296a439e89ed1a65225d9e358ec3b67e73fe3c
isuert <isuert.mjalti@adyen.com>
2024-03-12 11:54:31 +0100

    Remove log

--- packages/lib/src/components/external/Transactions/hooks/useMaxWidths.ts
+++ packages/lib/src/components/external/Transactions/hooks/useMaxWidths.ts
@@ -3,7 +3,6 @@ import { useCallback, useState } from 'preact/hooks';
 export const useMaxWidthsState = (): [number[], (widths: number[]) => void] => {
     const [maxWidths, setMaxWidths] = useState<number[]>([]);
     const setMaxWidthsConditionally = useCallback((widths: number[]) => {
-        console.log(widths);
         setMaxWidths(currentMaxWidths =>
             widths.map((width, index) => {
                 const currentMaxWidth = currentMaxWidths[index];
