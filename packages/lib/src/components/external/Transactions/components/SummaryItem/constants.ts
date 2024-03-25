95c645acf995383710101c0810e59a9c77fd954e
elifd <elif.dogali@adyen.com>
2024-03-25 10:31:17 +0100

    Change class names

--- packages/lib/src/components/external/Transactions/components/SummaryItem/constants.ts
+++ packages/lib/src/components/external/Transactions/components/SummaryItem/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_CLASS = 'adyen-fp-summary-item';
-export const LABEL_CONTAINER_CLASS = BASE_CLASS + '-container';
+export const LABEL_CONTAINER_CLASS = BASE_CLASS + '__label-container';
 export const BODY_CLASS = BASE_CLASS + '--body';
-export const LABEL_CLASS = LABEL_CONTAINER_CLASS + '__label';
+export const LABEL_CLASS = LABEL_CONTAINER_CLASS + '--text';
 export const PLACEHOLDER_CLASS = BASE_CLASS + '__placeholder';
 export const AMOUNT_CLASS = BASE_CLASS + '__amount';
