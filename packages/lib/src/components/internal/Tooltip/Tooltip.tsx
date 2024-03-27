d33f0708d91394eccc9de87999b787dea78fc297
elifd <elif.dogali@adyen.com>
2024-03-27 09:30:58 +0100

    Fix card header

--- packages/lib/src/components/internal/Tooltip/Tooltip.tsx
+++ packages/lib/src/components/internal/Tooltip/Tooltip.tsx
@@ -24,6 +24,8 @@ export const Tooltip = ({ content, children, triggerRef, showTooltip, position }
             {children
                 ? cloneElement(children, {
                       ...children?.props,
+                      role: 'button',
+                      tabIndex: 0,
                       ref: controllerRef,
                       className: children?.props?.className
                           ? `${children?.props?.className} adyen-fp-web__tooltip-target`
