106eca154668a8bf5280b234d4ca0aec5ca3c969
germanm <german.mora@adyen.com>
2024-03-31 19:50:20 +0200

    Fix amount formatting

--- packages/lib/src/components/internal/FilterBar/filters/AmountFilter/AmountFilter.tsx
+++ packages/lib/src/components/internal/FilterBar/filters/AmountFilter/AmountFilter.tsx
@@ -2,20 +2,45 @@ import BaseFilter from '../BaseFilter';
 import { FilterProps } from '../BaseFilter/types';
 import { RangeFilterProps } from './types';
 import { RangeSelection } from './RangeSelection';
-import { useCallback, useState } from 'preact/hooks';
+import { useCallback, useMemo, useState } from 'preact/hooks';
 import { EMPTY_OBJECT } from '@src/utils/common';
 import { PopoverContainerSize } from '@src/components/internal/Popover/types';
-
-const AMOUNT_CONSTANT = 100000;
+import useCoreContext from '@src/core/Context/useCoreContext';
+import { AMOUNT_CONSTANT } from './constants';
 
 export const AmountFilter = ({ updateFilters, selectedCurrencies, availableCurrencies, ...props }: FilterProps<RangeFilterProps>) => {
-    const [value, setValue] = useState<string | undefined>();
+    const { i18n } = useCoreContext();
+    const [value, setValue] = useState<{ minAmount: number | undefined; maxAmount: number | undefined } | undefined>();
+    const [formattedValue, setValueFormattedValue] = useState<string | undefined>();
+
+    const showCurrencySymbol = useMemo(() => {
+        if (selectedCurrencies?.length === 1 || availableCurrencies?.length === 1) return true;
+        if ((availableCurrencies?.length ?? 0) > 1) return false;
+        return false;
+    }, [availableCurrencies?.length, selectedCurrencies?.length]);
+
+    const formatAmount = useCallback(
+        (amount: number, showSymbol: boolean) => {
+            const currencyCode = selectedCurrencies?.[0] || availableCurrencies?.[0];
+            const formatter = new Intl.NumberFormat(i18n.locale);
+
+            if (!currencyCode) return formatter.format(amount);
+
+            return showSymbol
+                ? amount.toLocaleString(currencyCode, {
+                      style: 'currency',
+                      currency: currencyCode,
+                      currencyDisplay: 'symbol',
+                  })
+                : formatter.format(amount);
+        },
+        [availableCurrencies, i18n, selectedCurrencies]
+    );
 
     const onFilterChange = useCallback(
         (params: { minAmount: number | undefined; maxAmount: number | undefined; filterValue: string | undefined }) => {
-            setValue(params.filterValue);
-
             const { minAmount, maxAmount } = params ?? EMPTY_OBJECT;
+            setValue({ minAmount, maxAmount });
 
             updateFilters({
                 minAmount: minAmount !== undefined ? String(minAmount * AMOUNT_CONSTANT) : undefined,
@@ -25,6 +50,22 @@ export const AmountFilter = ({ updateFilters, selectedCurrencies, availableCurre
         [updateFilters]
     );
 
+    if (value && (value.minAmount || value.maxAmount)) {
+        const { minAmount, maxAmount } = value;
+
+        if (minAmount !== undefined && minAmount < 0) setValueFormattedValue(undefined);
+
+        if (minAmount !== undefined && maxAmount !== undefined) {
+            if (maxAmount < minAmount) setValueFormattedValue(undefined);
+            setValueFormattedValue(`${formatAmount(minAmount, showCurrencySymbol)} to ${formatAmount(maxAmount, showCurrencySymbol)}`);
+        } else {
+            if (minAmount !== undefined) setValueFormattedValue(`${i18n.get('from')} ${formatAmount(minAmount, showCurrencySymbol)}`);
+            if (maxAmount !== undefined) setValueFormattedValue(`${i18n.get('to')} ${formatAmount(maxAmount, showCurrencySymbol)}`);
+        }
+    } else {
+        setValueFormattedValue(undefined);
+    }
+
     return (
         <BaseFilter<RangeFilterProps>
             {...props}
@@ -32,8 +73,8 @@ export const AmountFilter = ({ updateFilters, selectedCurrencies, availableCurre
             minAmount={props.minAmount}
             maxAmount={props.maxAmount}
             onChange={onFilterChange}
-            value={value}
-            label={value ? value : props.label}
+            value={formattedValue}
+            label={formattedValue ? formattedValue : props.label}
             type={'text'}
             containerSize={PopoverContainerSize.MEDIUM}
             selectedCurrencies={selectedCurrencies}
