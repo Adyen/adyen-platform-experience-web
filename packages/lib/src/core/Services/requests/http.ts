2b25997453d2786903b37a7440e3c43d5b5faa92
elifd <elif.dogali@adyen.com>
2024-04-10 17:01:55 +0200

    Break our logic if the user sends an error handler function

--- packages/lib/src/core/Services/requests/http.ts
+++ packages/lib/src/core/Services/requests/http.ts
@@ -21,61 +21,64 @@ export async function http<T>(options: HttpOptions, data?: any): Promise<T> {
         const error = { level: errorLevel } as Parameters<typeof handleFetchError>[0];
         let errorPassThrough = false;
 
-        try {
-            /**
-             * There can be network error, abort, cors errors etc.
-             */
-            const res = await fetch(url, request); // (1)
-
-            if (res.ok) {
+        handle: {
+            try {
                 /**
-                 *  Response has no content body or body is not valid json this will fail
-                 *  We don't want to interfere the error so this will be propagated to the user
-                 *  We might need relabel the error like malformed content so that, user can distinguish from other errors
+                 * There can be network error, abort, cors errors etc.
                  */
-                errorPassThrough = true;
-                return await res.json(); // (!)
-            }
+                const res = await fetch(url, request); // (1)
 
-            error.type = getErrorType(res.status);
-            /**
-             *  Response has no content body or body is not valid json this will fail
-             *  We will handle this error since we have the errorCode
-             */
-            const response = await res.json(); // (2)
-
-            error.message = options.errorMessage || `Service at ${url} is not available`;
-            error.errorCode = String(response.status);
-            error.requestId = response?.requestId;
-
-            if (isAdyenErrorResponse(response)) {
-                if (options.errorHandler) {
+                if (res.ok) {
                     /**
-                     *  This will throw if the logic of consumer for the errorHanler results an error
+                     *  Response has no content body or body is not valid json this will fail
                      *  We don't want to interfere the error so this will be propagated to the user
+                     *  We might need relabel the error like malformed content so that, user can distinguish from other errors
                      */
                     errorPassThrough = true;
-                    options.errorHandler(response); // (!)
+                    return await res.json(); // (!)
                 }
 
-                error.message = response.detail;
-                error.errorCode = response.errorCode;
-            }
-        } catch (ex) {
-            if (errorPassThrough) {
+                error.type = getErrorType(res.status);
                 /**
-                 * This is the point that we are throwing an error for the places that we labeled as errorPassThrough
+                 *  Response has no content body or body is not valid json this will fail
+                 *  We will handle this error since we have the errorCode
                  */
-                throw ex;
-            }
-            error.message = options.errorMessage || `Call to ${url} failed. Error= ${ex}`;
-        }
+                const response = await res.json(); // (2)
 
-        /**
-         * This will throw an error if the level is error
-         * @see {handleFetchError}
-         */
-        handleFetchError(error);
+                error.message = options.errorMessage || `Service at ${url} is not available`;
+                error.errorCode = String(response.status);
+                error.requestId = response?.requestId;
+
+                if (isAdyenErrorResponse(response)) {
+                    if (options.errorHandler) {
+                        /**
+                         *  This will throw if the logic of consumer for the errorHanler results an error
+                         *  We don't want to interfere the error so this will be propagated to the user
+                         */
+                        errorPassThrough = true;
+                        options.errorHandler(response); // (!)
+                        break handle;
+                    }
+
+                    error.message = response.detail;
+                    error.errorCode = response.errorCode;
+                }
+            } catch (ex) {
+                if (errorPassThrough) {
+                    /**
+                     * This is the point that we are throwing an error for the places that we labeled as errorPassThrough
+                     */
+                    throw ex;
+                }
+                error.message = options.errorMessage || `Call to ${url} failed. Error= ${ex}`;
+            }
+
+            /**
+             * This will throw an error if the level is error
+             * @see {handleFetchError}
+             */
+            handleFetchError(error);
+        }
     })();
 }
 
