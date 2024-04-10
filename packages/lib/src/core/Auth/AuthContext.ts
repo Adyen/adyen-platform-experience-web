9f3e3d3d346bd059d99cec737c518b74f16cd2e8
elifd <elif.dogali@adyen.com>
2024-04-10 16:06:42 +0200

    Fix session error

--- packages/lib/src/core/Auth/AuthContext.ts
+++ packages/lib/src/core/Auth/AuthContext.ts
@@ -1,8 +1,9 @@
 import { AuthContextProps } from '@src/core/Auth/types';
 import { createContext } from 'preact';
-import { EMPTY_OBJECT } from '@src/utils/common';
+import { EMPTY_OBJECT, noop } from '@src/utils/common';
 
 export const AuthContext = createContext<AuthContextProps>({
     token: '',
     endpoints: EMPTY_OBJECT,
+    updateCore: noop,
 });
