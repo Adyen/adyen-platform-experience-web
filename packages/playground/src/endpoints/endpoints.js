12fa4b3588129fb345e9707791eb61c786efa339
elifd <elif.dogali@adyen.com>
2024-04-18 11:11:01 +0200

    Fix the mock endpoints

--- packages/playground/src/endpoints/endpoints.js
+++ packages/playground/src/endpoints/endpoints.js
@@ -3,11 +3,11 @@ export const endpoints = mode => {
     const prefix = mode === 'viteDev' ? '^' : 'https://platform-components-external-test.adyen.com/platform-components-external/api';
 
     return {
-        balanceAccount: `${prefix}/v([0-9]+)/balanceAccounts`,
-        balanceAccountTotals: `${prefix}/v([0-9]+)/balanceAccounts/${matchVariable}/transactions/totals`,
+        balanceAccounts: `${prefix}/v([0-9]+)/balanceAccounts`,
+        balanceAccountTotals: `${prefix}/v([0-9]+)/transactions/totals`,
         balances: `${prefix}/v([0-9]+)/balanceAccounts/${matchVariable}/balances`,
-        transactions: `${prefix}\/v([0-9]+)/balanceAccounts/${matchVariable}/transactions`,
-        transaction: `${prefix}/v([0-9]+)/balanceAccounts/transactions/${matchVariable}`,
+        transactions: `${prefix}/v([0-9]+)/transactions`,
+        transaction: `${prefix}/v([0-9]+)/transactions/${matchVariable}`,
         sessions: `\/api/authe/api/v1/sessions`,
         setup: `${prefix}/v([0-9]+)/setup`,
     };
