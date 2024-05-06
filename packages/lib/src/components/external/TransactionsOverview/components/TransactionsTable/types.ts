aea97b71ba58f500a3a45ccede91c581f4b29b81
elifd <elif.dogali@adyen.com>
2024-05-06 12:23:46 +0200

    Replace shared types and fix typo

--- packages/lib/src/components/external/TransactionsOverview/components/TransactionsTable/types.ts
+++ packages/lib/src/components/external/TransactionsOverview/components/TransactionsTable/types.ts
@@ -1,7 +1,6 @@
 import { PaginationProps, WithPaginationLimitSelection } from '@src/components/internal/Pagination/types';
-import { IBalanceAccountBase, ITransaction } from '@src/types';
+import { IBalanceAccountBase, ITransaction, OnSelection } from '@src/types';
 import AdyenPlatformExperienceError from '@src/core/Errors/AdyenPlatformExperienceError';
-import { OnSelection } from '@src/components';
 
 export interface TransactionTableProps extends WithPaginationLimitSelection<PaginationProps> {
     availableCurrencies: ITransaction['amount']['currency'][] | undefined;
--- packages/lib/src/components/external/TransactionsOverview/index.ts
+++ packages/lib/src/components/external/TransactionsOverview/index.ts
@@ -1,2 +1,1 @@
 export { default as TransactionsOverview } from './TransactionsOverviewElement';
-export * from './types';
