1fa1685cdf451d36c9986e729081daecf8c620d7
glad <glad.chinda@adyen.com>
2024-05-14 08:57:32 +0200

    Update lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+    'packages/lib/src/**/translations/*.json': filenames => [
+        `scripts/translations/sort-translations-json.sh ${filenames.join(' ')}`,
+    ],
     // Fix Prettier formatting
     // Don't include TS schemas
     '!(packages/lib/src/types/api/resources/**/*)/**/*.(ts|tsx|js|scss|css|md|json|html)': filenames => [
