c0f190b0209fff9a73a512cb31808abf163ac7a7
germanm <german.mora@adyen.com>
2024-05-20 14:07:15 +0200

    Include openapi types in the build

--- packages/lib/src/types/models/openapi/endpoints.ts
+++ packages/lib/src/types/models/openapi/endpoints.ts
@@ -10,4 +10,6 @@ export type SetupEndpoint = Record<EndpointName, SetupEndpointResponse>;
 
 export type EndpointsOperations = BalanceAccountOps & TransactionsOps;
 
-export type OperationParameters<Operation extends keyof EndpointsOperations> = EndpointsOperations[Operation]['parameters'];
+export type OperationParameters<Operation extends keyof EndpointsOperations> = EndpointsOperations[Operation] extends { parameters: infer P }
+    ? P
+    : never;
