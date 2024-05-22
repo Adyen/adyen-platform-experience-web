b3f624e47aaedb839ec8a4e7308543c823f64511
germanm <german.mora@adyen.com>
2024-05-22 14:52:56 +0200

    Remove unused schemas

--- packages/lib/src/types/models/openapi/endpoints.ts
+++ packages/lib/src/types/models/openapi/endpoints.ts
@@ -1,16 +1,14 @@
 import { operations as BalanceAccountOps } from './BalanceAccountsResource';
 import { operations as TransactionsOps } from './TransactionsResource';
-import { operations as PaymentInstrumentsOps } from './PaymentInstrumentsResource';
-import { operations as PayoutOps } from './PayoutsResource';
 import { components as SetupResource } from './SetupResource';
 
-export type EndpointName = SetupResource['schemas']['EndpointName'];
+export type EndpointsOperations = BalanceAccountOps & TransactionsOps;
+
+export type EndpointName = Extract<keyof EndpointsOperations, SetupResource['schemas']['EndpointName']>;
 
 export type SetupEndpointResponse = SetupResource['schemas']['SetupEndpointResponse'];
 
 export type SetupEndpoint = Record<EndpointName, SetupEndpointResponse>;
-
-export type EndpointsOperations = BalanceAccountOps & PaymentInstrumentsOps & PayoutOps & TransactionsOps;
 
 export type OperationParameters<Operation extends keyof EndpointsOperations> = EndpointsOperations[Operation] extends { parameters: infer P }
     ? P
