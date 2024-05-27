c6c81e59d0930a4d745b914251410358c0917f6e
germanm <german.mora@adyen.com>
2024-05-27 11:33:30 +0200

    Remove Theme

--- packages/lib/src/components/external/TransactionsOverview/components/TransactionsOverviewContainer/TransactionsOverviewContainer.tsx
+++ packages/lib/src/components/external/TransactionsOverview/components/TransactionsOverviewContainer/TransactionsOverviewContainer.tsx
@@ -8,17 +8,6 @@ import Theme from '../../../../../theme/Theme';
 function TransactionsOverviewContainer({ ...props }: ExternalUIComponentProps<DataOverviewComponentProps>) {
     const { balanceAccounts, isBalanceAccountIdWrong, isFetching, error } = useBalanceAccounts(props.balanceAccountId);
 
-    /*    $my-neutral-scale: generate-grey-scale(#ffe6c9, 0);
-    $my-primary-scale: generate-color-scale(#8b2f00, 32);
-    $my-label-scale: generate-color-scale(#1d1d1d, 32);
-    $my-primary-background: #fffaf8;
-
-    $my-critical-scale: generate-color-scale(#f00, 19);
-    $my-success-scale: generate-color-scale(#25c000, 17);
-    $my-warning-scale: generate-color-scale(#25c000, 17);*/
-
-    new Theme({ primary: '#8b2f00', neutral: '#ffe6c9', label: '#1d1d1d' /*outline: '#ff0000'*/ }).apply();
-
     return (
         <DataOverviewContainer
             balanceAccountsError={error}
