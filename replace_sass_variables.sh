ff4a558469e9f35f4455e183274838b4c53b9d7d
german <german.mora@adyen.com>
2024-05-27 05:54:26 -0500

    Replace border radius variables

--- replace_sass_variables.sh
+++ replace_sass_variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Find all .scss and .sass files and replace the variable pattern using sed
-find . -type f \( -name "*.scss" -o -name "*.sass" \) -print0 | xargs -0 sed -i -E 's/\$color-([a-zA-Z0-9_-]+)/token(color-\1)/g'
+find . -type f \( -name "*.scss" -o -name "*.sass" \) -print0 | xargs -0 sed -i -E 's/var\(--adyen-sdk-width-([a-zA-Z0-9_-]+)\)/token(border-width-\1)/g'
