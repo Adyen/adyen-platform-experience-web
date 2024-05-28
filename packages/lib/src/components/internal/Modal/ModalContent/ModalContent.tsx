4ac8829ee9fec5cd16386e40c84cf07d35d46d82
elifd <elif.dogali@adyen.com>
2024-05-28 17:40:22 +0200

    Rename the function to be more generic

--- packages/lib/src/components/internal/Modal/ModalContent/ModalContent.tsx
+++ packages/lib/src/components/internal/Modal/ModalContent/ModalContent.tsx
@@ -3,9 +3,9 @@ import { TransactionDetailData } from '../../../external';
 import DataOverviewDetails from '../../DataOverviewDetails/DataOverviewDetails';
 import { SelectedDetail } from '../../DataOverviewDetails/types';
 
-const isTransactionWithoutId = (data: string | TransactionDetailData): data is TransactionDetailData => hasOwnProperty(data, 'id');
+const isDetailsWithoutId = (data: string | TransactionDetailData): data is TransactionDetailData => hasOwnProperty(data, 'id');
 function ModalContent({ type, data }: SelectedDetail) {
-    const detailProps = isTransactionWithoutId(data) ? { data: data } : { id: data };
+    const detailProps = isDetailsWithoutId(data) ? { data: data } : { id: data };
 
     return <>{detailProps && <DataOverviewDetails {...detailProps} type={type} />}</>;
 }
