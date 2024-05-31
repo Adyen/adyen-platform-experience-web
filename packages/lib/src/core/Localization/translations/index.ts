7e727ea868366ee289e317e2654f83ba45e42376
pablo <pablo.igarzabal@adyen.com>
2024-05-31 16:10:52 +0200

    Delete unused translation files

--- packages/lib/src/core/Localization/translations/index.ts
+++ packages/lib/src/core/Localization/translations/index.ts
@@ -1,80 +1,8 @@
 import _en_US from './en-US.json';
-import _ar from './ar.json';
-import _cs_CZ from './cs-CZ.json';
-import _da_DK from './da-DK.json';
-import _de_DE from './de-DE.json';
-import _el_GR from './el-GR.json';
 import _es_ES from './es-ES.json';
-import _fi_FI from './fi-FI.json';
-import _fr_FR from './fr-FR.json';
-import _hr_HR from './hr-HR.json';
-import _hu_HU from './hu-HU.json';
-import _it_IT from './it-IT.json';
-import _ja_JP from './ja-JP.json';
-import _ko_KR from './ko-KR.json';
-import _nl_NL from './nl-NL.json';
-import _no_NO from './no-NO.json';
-import _pl_PL from './pl-PL.json';
-import _pt_BR from './pt-BR.json';
-import _pt_PT from './pt-PT.json';
-import _ro_RO from './ro-RO.json';
-import _ru_RU from './ru-RU.json';
-import _sk_SK from './sk-SK.json';
-import _sl_SI from './sl-SI.json';
-import _sv_SE from './sv-SE.json';
-import _zh_CN from './zh-CN.json';
-import _zh_TW from './zh-TW.json';
 
 export const en_US = { en_US: _en_US };
-export const ar = { ar: _ar };
-export const cs_CZ = { cs_CZ: _cs_CZ };
-export const da_DK = { da_DK: _da_DK };
-export const de_DE = { de_DE: _de_DE };
-export const el_GR = { el_GR: _el_GR };
 export const es_ES = { es_ES: _es_ES };
-export const fi_FI = { fi_FI: _fi_FI };
-export const fr_FR = { fr_FR: _fr_FR };
-export const hr_HR = { hr_HR: _hr_HR };
-export const hu_HU = { hu_HU: _hu_HU };
-export const it_IT = { it_IT: _it_IT };
-export const ja_JP = { ja_JP: _ja_JP };
-export const ko_KR = { ko_KR: _ko_KR };
-export const nl_NL = { nl_NL: _nl_NL };
-export const no_NO = { no_NO: _no_NO };
-export const pl_PL = { pl_PL: _pl_PL };
-export const pt_BR = { pt_BR: _pt_BR };
-export const pt_PT = { pt_PT: _pt_PT };
-export const ro_RO = { ro_RO: _ro_RO };
-export const ru_RU = { ru_RU: _ru_RU };
-export const sk_SK = { sk_SK: _sk_SK };
-export const sl_SI = { sl_SI: _sl_SI };
-export const sv_SE = { sv_SE: _sv_SE };
-export const zh_CN = { zh_CN: _zh_CN };
-export const zh_TW = { zh_TW: _zh_TW };
 export const all_locales = {
-    zh_TW: _zh_TW,
-    zh_CN: _zh_CN,
-    sv_SE: _sv_SE,
-    sl_SI: _sl_SI,
-    sk_SK: _sk_SK,
-    ru_RU: _ru_RU,
-    ro_RO: _ro_RO,
-    pt_PT: _pt_PT,
-    pt_BR: _pt_BR,
-    pl_PL: _pl_PL,
-    no_NO: _no_NO,
-    nl_NL: _nl_NL,
-    ko_KR: _ko_KR,
-    ja_JP: _ja_JP,
-    it_IT: _it_IT,
-    hu_HU: _hu_HU,
-    hr_HR: _hr_HR,
-    fr_FR: _fr_FR,
-    fi_FI: _fi_FI,
     es_ES: _es_ES,
-    el_GR: _el_GR,
-    de_DE: _de_DE,
-    da_DK: _da_DK,
-    cs_CZ: _cs_CZ,
-    ar: _ar,
 };
