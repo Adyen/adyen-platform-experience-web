eb51bd04b13b6eaf2aef0c46bffa13e2a127dcb8
germanm <german.mora@adyen.com>
2024-06-04 11:42:39 +0200

    fix/schemas-script

--- scripts/generate-ts-schemas/generate-schemas.sh
+++ scripts/generate-ts-schemas/generate-schemas.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+EXCLUDED_RESOURCES=("PaymentInstrumentsResource")
+
 # Get root dir of the project
 SCRIPT_DIR=$(dirname "$0")
 PROJECT_ROOT_DIR=$(realpath "$SCRIPT_DIR/../..")
@@ -32,6 +34,13 @@ CA_CERTS="$CA_CERTS"
 
 BRANCH_NAME="main"
 
+excluded_folders=()
+
+for ((i=0; i<${#EXCLUDED_RESOURCES[@]}; i++)); do
+  resource=${EXCLUDED_RESOURCES[i]}
+  excluded_folders+=("$FOLDER_PATH/$resource")
+done
+
 # Function to URL-encode the folder path
 urlencode() {
     echo -n "$1" | perl -MURI::Escape -ne 'print uri_escape($_)';
@@ -58,26 +67,34 @@ file_names=($(echo "$response_body" | jq -r '.[] | select(.type == "tree") | .na
 # Loop over the array of folders
 for ((i=0; i<${#file_folders[@]}; i++)); do
 
-    FOLDER_URL="https://$REPO_URL/api/v4/projects/$PROJECT_ID/repository/tree?path=${file_folders[i]}&ref=$BRANCH_NAME"
+    folder=${file_folders[i]}
 
-    response=$(curl -s --header "PRIVATE-TOKEN: $API_TOKEN" "$FOLDER_URL")
-    files=($(echo "$response" | jq -r '.[] | select(.type == "blob") | .path'))
+    if [[ ${excluded_folders[@]} =~ $folder ]]; then
+      echo "$folder is excluded"
+    else
 
-    # Find correct version
-    version_file_path=()
-    for file in "${files[@]}"; do
-        if [[ $file == *"$PACKAGE_VERSION"* ]]; then
-            version_file_path+=("$file")
-        fi
-    done
+      FOLDER_URL="https://$REPO_URL/api/v4/projects/$PROJECT_ID/repository/tree?path=${folder}&ref=$BRANCH_NAME"
 
-    ENCODED_FOLDER_PATH=$(urlencode "$version_file_path")
+      response=$(curl -s --header "PRIVATE-TOKEN: $API_TOKEN" "$FOLDER_URL")
+      files=($(echo "$response" | jq -r '.[] | select(.type == "blob") | .path'))
 
-    FILE_URL="https://$REPO_URL/api/v4/projects/$PROJECT_ID/repository/files/$ENCODED_FOLDER_PATH/raw?ref=$BRANCH_NAME"
+      # Find correct version
+      version_file_path=()
+      for file in "${files[@]}"; do
+          if [[ $file == *"$PACKAGE_VERSION"* ]]; then
+              version_file_path+=("$file")
+          fi
+      done
 
-    export NODE_EXTRA_CA_CERTS="$CA_CERTS"
+      ENCODED_FOLDER_PATH=$(urlencode "$version_file_path")
 
-    #Generate schemas
-    npx openapi-typescript "$FILE_URL" -o "${SCHEMAS_DIR}/${file_names[i]}.ts" --header "PRIVATE-TOKEN: $API_TOKEN"
+      FILE_URL="https://$REPO_URL/api/v4/projects/$PROJECT_ID/repository/files/$ENCODED_FOLDER_PATH/raw?ref=$BRANCH_NAME"
+
+      export NODE_EXTRA_CA_CERTS="$CA_CERTS"
+
+      #Generate schemas
+      npx openapi-typescript "$FILE_URL" -o "${SCHEMAS_DIR}/${file_names[i]}.ts" --header "PRIVATE-TOKEN: $API_TOKEN"
+
+    fi
 
 done
