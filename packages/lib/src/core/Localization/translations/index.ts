6c95447eb3a95812a29b3b9be72ad0443b818668
pablo <pablo.igarzabal@adyen.com>
2024-06-10 18:02:11 +0200

    Delete unused file which was added incorrectly on a merge

--- packages/lib/src/core/Localization/translations/index.ts
+++
@@ -1,8 +0,0 @@
-import _en_US from './en-US.json';
-import _es_ES from './es-ES.json';
-
-export const en_US = { en_US: _en_US };
-export const es_ES = { es_ES: _es_ES };
-export const all_locales = {
-    es_ES: _es_ES,
-};
