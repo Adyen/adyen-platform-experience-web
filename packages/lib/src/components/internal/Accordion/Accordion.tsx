28b7e6c9a60bc2636df033c508b61c72555ff98d
elifd <elif.dogali@adyen.com>
2024-07-09 17:43:11 +0200

    Add accordion animation

--- packages/lib/src/components/internal/Accordion/Accordion.tsx
+++ packages/lib/src/components/internal/Accordion/Accordion.tsx
@@ -1,6 +1,7 @@
+import cx from 'classnames';
 import classnames from 'classnames';
 import { PropsWithChildren } from 'preact/compat';
-import { useCallback, useState } from 'preact/hooks';
+import { useCallback, useRef, useState } from 'preact/hooks';
 import ChevronDown from '../SVGIcons/ChevronDown';
 import ChevronUp from '../SVGIcons/ChevronUp';
 import {
@@ -15,6 +16,7 @@ import './Accordion.scss';
 
 function Accordion({ children, classNames, header, headerInformation }: PropsWithChildren<AccordionProps>) {
     const [isExpanded, setIsExpanded] = useState(false);
+    const accordionContentRef = useRef<HTMLDivElement>(null);
 
     const toggle = useCallback(() => {
         setIsExpanded(!isExpanded);
@@ -37,11 +39,17 @@ function Accordion({ children, classNames, header, headerInformation }: PropsWit
                 </button>
                 {headerInformation && <div>{headerInformation}</div>}
             </h3>
-            {isExpanded && (
-                <div role="region" id={'accordion-content'} aria-labelledby="accordion-controller" className={ACCORDION_CONTENT_CLASS}>
-                    {children}
+            {
+                <div
+                    role="region"
+                    id={'accordion-content'}
+                    aria-labelledby="accordion-controller"
+                    style={{ maxHeight: isExpanded ? accordionContentRef?.current?.offsetHeight : 0 }}
+                    className={ACCORDION_CONTENT_CLASS}
+                >
+                    <div ref={accordionContentRef}>{children}</div>
                 </div>
-            )}
+            }
         </div>
     );
 }
