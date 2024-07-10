9c7400afaeefa0fb81b448039c47dfd04fd0c0e0
glad <glad.chinda@adyen.com>
2024-07-10 16:07:40 +0200

    Added autorefresher test suite

--- packages/lib/src/primitives/context/session/internal/autofresher.ts
+++ packages/lib/src/primitives/context/session/internal/autofresher.ts
@@ -1,25 +1,27 @@
-import { boolOrFalse, falsify, isFunction, noop, tryResolve } from '../../../../utils';
+import { boolOrFalse, falsify, getter, isFunction, noop, tryResolve } from '../../../../utils';
 import { createPromisor } from '../../../async/promisor';
 import { EVT_SESSION_EXPIRED } from '../constants';
 import type { SessionRefresher } from './types';
 
+export const _canAutofresh = async <T extends any>(refresher: SessionRefresher<T>) => {
+    const { specification } = refresher.context;
+
+    const canAutofresh = await tryResolve(async () => {
+        const _autoRefresh = specification.autoRefresh;
+        return isFunction(_autoRefresh) ? _autoRefresh.call(specification, refresher.session) : _autoRefresh;
+    }).catch(falsify);
+
+    return boolOrFalse(canAutofresh);
+};
+
 export const createSessionAutofresher = <T extends any>(refresher: SessionRefresher<T>) => {
-    const { emitter, specification } = refresher.context;
+    let _unlistenExpired = refresher.context.emitter.on(EVT_SESSION_EXPIRED, () => _autofresh(false));
     let _autofreshSignal: AbortSignal | undefined;
 
-    const _canAutofresh = async () => {
-        const canAutofresh = await tryResolve(async () => {
-            const _autoRefresh = specification.autoRefresh;
-            return isFunction(_autoRefresh) ? _autoRefresh.call(specification, refresher.session) : _autoRefresh;
-        }).catch(falsify);
-
-        return boolOrFalse(canAutofresh);
-    };
-
-    const _autofreshPromisor = createPromisor(async (signal, skipCanAutofreshCheck = false) => {
+    let _autofreshPromisor = createPromisor(async (signal, skipCanAutofreshCheck = false) => {
         _autofreshSignal = signal;
 
-        const canAutofresh = boolOrFalse(skipCanAutofreshCheck) || (await _canAutofresh());
+        const canAutofresh = boolOrFalse(skipCanAutofreshCheck) || (await _canAutofresh(refresher));
 
         if (_autofreshSignal !== signal) {
             // The current autofresh signal does not match the autofresh attempt `signal`,
@@ -35,12 +37,22 @@ export const createSessionAutofresher = <T extends any>(refresher: SessionRefres
         }
     });
 
-    const autofresh = (skipCanAutofreshCheck = false) => {
+    let _autofresh = (skipCanAutofreshCheck = false) => {
         if (!refresher.refreshing) void _autofreshPromisor(skipCanAutofreshCheck);
     };
 
-    emitter.on(EVT_SESSION_EXPIRED, autofresh.bind(null, false));
-    return autofresh;
+    let _destruct = () => {
+        _unlistenExpired();
+        _autofreshPromisor.abort();
+        _autofreshPromisor = _autofreshSignal = _unlistenExpired = undefined!;
+        _autofresh = _destruct = noop;
+    };
+
+    const autofresh = (skipCanAutofreshCheck = false) => _autofresh(skipCanAutofreshCheck);
+
+    return Object.defineProperties(autofresh as typeof autofresh & { readonly destruct: () => void }, {
+        destruct: getter(() => _destruct, false),
+    });
 };
 
 export default createSessionAutofresher;
