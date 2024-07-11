ff9bb353420bbaab4f4579098abda6a3679d4362
glad <glad.chinda@adyen.com>
2024-07-11 16:32:26 +0200

    Update playground locales selection

--- packages/playground/src/pages/transaction/Transaction.ts
+++ packages/playground/src/pages/transaction/Transaction.ts
@@ -1,24 +1,30 @@
-import { AdyenPlatformExperience, TransactionsDetails } from '@adyen/adyen-platform-experience-web';
-import { getDefaultID, getSearchParameters } from '../../utils/utils';
+import { AdyenPlatformExperience, all_locales, TransactionsDetails } from '@adyen/adyen-platform-experience-web';
+import sessionRequest from '../../utils/sessionRequest';
 import '../../utils/createPages.js';
 import '../../assets/style/style.scss';
+
 import { enableServerInMockedMode } from '../../endpoints/mock-server/utils';
-import sessionRequest from '../../utils/sessionRequest';
+import { createLanguageButtons } from '../../utils/createLanguageButtons';
+import { getDefaultID, getSearchParameters } from '../../utils/utils';
 
 const DEFAULT_TRANSACTION_ID = getDefaultID('1VVF0D5V3709DX6D');
 
 enableServerInMockedMode()
     .then(async () => {
         const { id } = getSearchParameters();
-        const AdyenPlatform = await AdyenPlatformExperience({
-            locale: 'en-US',
+
+        const core = await AdyenPlatformExperience({
+            environment: 'test',
+            availableTranslations: [all_locales],
             async onSessionCreate() {
                 return await sessionRequest();
             },
         });
 
+        createLanguageButtons({ core });
+
         const transactionsDetailsComponent = new TransactionsDetails({
-            core: AdyenPlatform,
+            core,
             id: id ?? DEFAULT_TRANSACTION_ID,
             title: 'transactionDetails',
             onContactSupport: () => {},
