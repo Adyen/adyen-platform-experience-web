6128709ca859fa60e3e8ca0adeb3e0ee2f9c85ae
germanm <german.mora@adyen.com>
2024-07-24 12:43:15 +0200

    Remove resource file

--- src/types/api/endpoints.ts
+++ src/types/api/endpoints.ts
@@ -1,11 +1,10 @@
 import { operations as BalanceAccountOps } from './resources/BalanceAccountsResource';
-import { operations as PaymentInstrumentsOps } from './resources/PaymentInstrumentsResource';
 import { operations as PayoutsOps } from './resources/PayoutsResource';
 import { operations as TransactionsOps } from './resources/TransactionsResource';
 import { operations as ReportsOps } from './resources/ReportsResource';
 import { components as SetupResource } from './resources/SetupResource';
 
-export type EndpointsOperations = BalanceAccountOps & PaymentInstrumentsOps & PayoutsOps & TransactionsOps & ReportsOps;
+export type EndpointsOperations = BalanceAccountOps & PayoutsOps & TransactionsOps & ReportsOps;
 
 export type EndpointName = Extract<keyof EndpointsOperations, SetupResource['schemas']['EndpointName']>;
 
