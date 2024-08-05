56ac46ab3c2f1b26963780d763c9c7f5ba6bb649
glad <glad.chinda@adyen.com>
2024-08-05 17:51:20 +0200

    Move TextArea component to separate file

--- src/components/external/TransactionDetails/components/TransactionRefundReference.tsx
+++ src/components/external/TransactionDetails/components/TransactionRefundReference.tsx
@@ -8,80 +8,13 @@ import {
     TX_DATA_INPUT_TEXTAREA,
 } from '../constants';
 import { h } from 'preact';
-import { useCallback, useLayoutEffect, useRef, useState } from 'preact/hooks';
+import { useState } from 'preact/hooks';
 import { useInputNormalizer } from '../context/useInputNormalizer';
 import { REFUND_REFERENCE_CHAR_LIMIT, useTransactionDataContext } from '../context';
 import useCoreContext from '../../../../core/Context/useCoreContext';
+import TextArea from '../../../internal/FormFields/TextArea';
 import Typography from '../../../internal/Typography/Typography';
 import { TypographyElement, TypographyVariant } from '../../../internal/Typography/types';
-import { EMPTY_OBJECT, isUndefined } from '../../../../utils';
-import cx from 'classnames';
-
-const TextArea = (props: h.JSX.HTMLAttributes<HTMLTextAreaElement> & { maxRows?: number }) => {
-    const { className, maxRows, onInput, rows, ...externalProps } = props;
-    const { inputRef, recomputeInputSize } = useAutoResizableTextArea({ maxRows, minRows: rows as number });
-
-    const autoResizeOnInput = useCallback(
-        (evt: h.JSX.TargetedInputEvent<HTMLTextAreaElement>) => {
-            onInput?.(evt);
-            if (inputValue.current !== evt.currentTarget.value) {
-                inputValue.current = evt.currentTarget.value;
-                recomputeInputSize();
-            }
-        },
-        [onInput, recomputeInputSize]
-    );
-
-    const autoResizable = !isUndefined(maxRows);
-    const inputValue = useRef('');
-
-    return (
-        <div
-            className={cx('adyen-pe-input-wrapper', {
-                'adyen-pe-input-wrapper--autoresizable-textarea': autoResizable,
-            })}
-        >
-            <textarea
-                {...externalProps}
-                {...(!autoResizable && { rows })}
-                onInput={autoResizable ? autoResizeOnInput : onInput}
-                className={cx('adyen-pe-input', className)}
-                ref={inputRef}
-            />
-        </div>
-    );
-};
-
-const useAutoResizableTextArea = ({ maxRows, minRows = 1 } = EMPTY_OBJECT as { maxRows?: number; minRows?: number }) => {
-    const [lastUpdated, setLastUpdated] = useState(performance.now());
-    const inputRef = useRef<HTMLTextAreaElement>(null);
-    const inputOriginHeight = useRef(0);
-    const inputLineHeight = useRef(0);
-
-    const recomputeInputSize = useCallback(() => {
-        setLastUpdated(performance.now());
-        inputRef.current && (inputRef.current.rows = minRows);
-    }, [minRows, maxRows]);
-
-    useLayoutEffect(recomputeInputSize, [recomputeInputSize]);
-
-    useLayoutEffect(() => {
-        if (inputRef.current) {
-            const input = inputRef.current;
-            const currentHeight = input.scrollHeight;
-
-            if (!inputOriginHeight.current || maxRows === undefined) {
-                input.style.marginRight = `${input.scrollWidth - input.offsetWidth}px`;
-            }
-
-            inputOriginHeight.current ||= currentHeight;
-            inputLineHeight.current ||= parseInt(getComputedStyle(input).lineHeight, 10) || 0;
-            input.rows = Math.min(maxRows ?? Infinity, Math.floor((currentHeight - inputOriginHeight.current) / inputLineHeight.current) + 1);
-        }
-    }, [maxRows, lastUpdated]);
-
-    return { inputRef, recomputeInputSize };
-};
 
 const TransactionRefundReference = () => {
     const { i18n } = useCoreContext();
@@ -94,7 +27,7 @@ const TransactionRefundReference = () => {
     const onInput = (evt: h.JSX.TargetedInputEvent<HTMLTextAreaElement>) => {
         const textarea = evt.currentTarget;
         const selectionEnd = textarea.selectionEnd;
-        const value = inputNormalizer(textarea.value);
+        const value = inputNormalizer(textarea.value); // [TODO]: Confirm reference validation from backend
 
         textarea.value = value;
         textarea.setSelectionRange(selectionEnd, selectionEnd);
@@ -119,21 +52,13 @@ const TransactionRefundReference = () => {
 
             <div className={`${TX_DATA_INPUT_CONTAINER} ${TX_DATA_INPUT_CONTAINER_TEXT}`}>
                 <label htmlFor="refundReference">
-                    {/*<textarea*/}
-                    {/*    ref={inputRef}*/}
-                    {/*    className={`adyen-pe-input ${TX_DATA_INPUT} ${TX_DATA_INPUT_TEXTAREA}`}*/}
-                    {/*    id="refundReference"*/}
-                    {/*    placeholder={i18n.get('refundReference.placeholder')}*/}
-                    {/*    onInput={onInput}*/}
-                    {/*    value={reference}*/}
-                    {/*/>*/}
                     <TextArea
                         id="refundReference"
                         className={`${TX_DATA_INPUT} ${TX_DATA_INPUT_TEXTAREA}`}
                         placeholder={i18n.get('refundReference.placeholder')}
                         value={reference}
                         onInput={onInput}
-                        maxRows={4}
+                        maxRows={2}
                     />
                 </label>
             </div>
