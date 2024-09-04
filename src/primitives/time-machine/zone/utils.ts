fb6626762680a6c90efc1be8ed8604f88584ac12
glad <glad.chinda@adyen.com>
2024-09-04 14:35:17 +0200

    refactor(zone): Modify DateTimeFormat options retrieval

--- src/primitives/time-machine/zone/utils.ts
+++ src/primitives/time-machine/zone/utils.ts
@@ -4,25 +4,35 @@ import type { DateValue } from '../../../utils/datetime/types';
 import type { TimezoneDataSource } from '../types';
 
 const LOCALE = 'en-US';
-const DIGITS_2 = '2-digit';
 const TZ_OFFSET_REGEXP = /(?<=GMT)(?:([-+](?:0?\d|1[0-4]))(?::?([0-5]\d))?)?$/;
 
-const DATE_TIME_FORMAT_OPTIONS: Readonly<Intl.DateTimeFormatOptions> = Object.freeze({
-    weekday: 'short',
-    year: 'numeric',
-    month: DIGITS_2,
-    day: DIGITS_2,
-    hour: DIGITS_2,
-    minute: DIGITS_2,
-    second: DIGITS_2,
-    fractionalSecondDigits: 3,
-    hour12: false,
-    timeZoneName: 'longOffset', // do not change (required for `TZ_OFFSET_REGEXP`)
-});
+const _getDateTimeFormatOptions = (() => {
+    let tz: string | undefined;
+    const DIGITS_2 = '2-digit';
+    const FORMAT_OPTIONS: Readonly<Intl.DateTimeFormatOptions> = Object.freeze({
+        weekday: 'short',
+        year: 'numeric',
+        month: DIGITS_2,
+        day: DIGITS_2,
+        hour: DIGITS_2,
+        minute: DIGITS_2,
+        second: DIGITS_2,
+        fractionalSecondDigits: 3,
+        hour12: false,
+        timeZoneName: 'longOffset',
+        // prettier-ignore
+        get timeZone() { return tz },
+    });
+
+    return (timezone?: string) => {
+        tz = timezone;
+        return FORMAT_OPTIONS;
+    };
+})();
 
 export const dateFrom = (date: DateValue) => new Date(parseDate(date)!);
 export const getResolvedTimezone = (timezone?: string) => getTimezoneFormatter(timezone).resolvedOptions().timeZone;
-export const getTimezoneFormatter = (timeZone?: string) => new Intl.DateTimeFormat(LOCALE, { ...DATE_TIME_FORMAT_OPTIONS, timeZone });
+export const getTimezoneFormatter = (timezone?: string) => new Intl.DateTimeFormat(LOCALE, _getDateTimeFormatOptions(timezone));
 
 export function getTimezoneUTCOffset(this: TimezoneDataSource, date: Date) {
     const [offsetHrs = '', offsetMins = ''] = matchTimezoneFormattedString.call(this, TZ_OFFSET_REGEXP, date);
