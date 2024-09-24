65d0fadabef97becc47d95115195b93402c3fa9c
isuert <isuert.mjalti@adyen.com>
2024-09-24 18:18:04 +0200

    Introduce Header in CapitalOffer component

--- src/components/external/CapitalOffer/components/CapitalOffer/CapitalOffer.tsx
+++ src/components/external/CapitalOffer/components/CapitalOffer/CapitalOffer.tsx
@@ -1,25 +1,18 @@
 import { FunctionalComponent } from 'preact';
 import { useState } from 'preact/hooks';
-import useCoreContext from '../../../../../core/Context/useCoreContext';
-import Typography from '../../../../internal/Typography/Typography';
-import { TypographyVariant } from '../../../../internal/Typography/types';
 import { ExternalUIComponentProps } from '../../../../types';
 import { CapitalOfferProps } from '../../types';
 import { CAPITAL_OFFER_CLASS_NAMES } from './constants';
+import { CapitalHeader } from '../../../../internal/CapitalHeader';
 
 type CapitalOfferState = 'OfferSelection' | 'OfferSummary';
 
 export const CapitalOffer: FunctionalComponent<ExternalUIComponentProps<CapitalOfferProps>> = ({ hideTitle }) => {
-    const { i18n } = useCoreContext();
     const [state] = useState<CapitalOfferState>('OfferSelection');
 
     return (
         <div className={CAPITAL_OFFER_CLASS_NAMES.base}>
-            {!hideTitle && (
-                <Typography variant={TypographyVariant.TITLE} medium className={CAPITAL_OFFER_CLASS_NAMES.title}>
-                    {i18n.get(state === 'OfferSummary' ? 'capital.grantOfferSummary' : 'capital.grantOffer')}
-                </Typography>
-            )}
+            <CapitalHeader hideTitle={hideTitle} titleKey={state === 'OfferSummary' ? 'capital.grantOfferSummary' : 'capital.grantOffer'} />
         </div>
     );
 };
--- src/components/external/CapitalOffer/components/CapitalOffer/constants.ts
+++ src/components/external/CapitalOffer/components/CapitalOffer/constants.ts
@@ -1,4 +1,3 @@
 export const CAPITAL_OFFER_CLASS_NAMES = {
     base: 'adyen-pe-capital-offer',
-    title: 'adyen-pe-capital-offer__title',
 };
