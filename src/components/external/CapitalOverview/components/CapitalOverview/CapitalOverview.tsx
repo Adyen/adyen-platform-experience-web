f95329b29e7d9c16d104435df1e822a363b1ee10
isuert <isuert.mjalti@adyen.com>
2024-09-30 16:11:06 +0200

    Introduce grant list

--- src/components/external/CapitalOverview/components/CapitalOverview/CapitalOverview.tsx
+++ src/components/external/CapitalOverview/components/CapitalOverview/CapitalOverview.tsx
@@ -1,14 +1,17 @@
 import { useMemo } from 'preact/hooks';
-import { ExternalUIComponentProps } from '../../../types';
-import { CapitalOverviewProps } from '../types';
-import { CAPITAL_OVERVIEW_CLASS_NAMES } from '../constants';
+import { ExternalUIComponentProps } from '../../../../types';
+import { CapitalOverviewProps } from '../../types';
+import { CAPITAL_OVERVIEW_CLASS_NAMES } from '../../constants';
 import { FunctionalComponent } from 'preact';
-import { useAuthContext } from '../../../../core/Auth';
-import { useFetch } from '../../../../hooks/useFetch';
-import { EMPTY_OBJECT } from '../../../../utils';
-import { CapitalHeader } from '../../../internal/CapitalHeader';
+import { useAuthContext } from '../../../../../core/Auth';
+import { useFetch } from '../../../../../hooks/useFetch';
+import { EMPTY_OBJECT } from '../../../../../utils';
+import { CapitalHeader } from '../../../../internal/CapitalHeader';
+import { BaseList } from '../../../../internal/BaseList/BaseList';
+import { GrantItem } from '../GrantItem/GrantItem';
+import './CapitalOverview.scss';
 
-export const CapitalOverview: FunctionalComponent<ExternalUIComponentProps<CapitalOverviewProps>> = ({ hideTitle, skipPreQualifiedIntro }) => {
+export const CapitalOverview: FunctionalComponent<ExternalUIComponentProps<CapitalOverviewProps>> = ({ hideTitle }) => {
     const { getGrants: grantsEndpointCall, getDynamicGrantOffersConfiguration: dynamicConfigurationEndpointCall } = useAuthContext().endpoints;
 
     const grantsQuery = useFetch(
@@ -40,9 +43,15 @@ export const CapitalOverview: FunctionalComponent<ExternalUIComponentProps<Capit
 
     return (
         <div className={CAPITAL_OVERVIEW_CLASS_NAMES.base}>
-            <CapitalHeader hideTitle={hideTitle} titleKey={grantList?.length ? 'capital.grants' : 'capital.grantOffer'} />
+            <CapitalHeader hideTitle={hideTitle} titleKey={'capital.financialOffer'} />
             {grantList?.length ? (
-                <div>{'Placeholder for grants list'}</div>
+                <BaseList>
+                    {grantList.map(grant => (
+                        <li key={grant.id}>
+                            <GrantItem grant={grant} />
+                        </li>
+                    ))}
+                </BaseList>
             ) : dynamicOffer?.maxAmount ? (
                 <div>{'Placeholder for prequalified component'}</div>
             ) : null}
