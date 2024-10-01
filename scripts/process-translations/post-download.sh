e616dcadb6fe46d068861599006b352a3cd71a64
glad <glad.chinda@adyen.com>
2024-10-01 17:20:00 +0200

    fix: download translations workflow

---
+++ scripts/process-translations/post-download.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!! NEVER RUN THIS SHELL SCRIPT YOURSELF !!#
+#!! WILL RUN AUTOMATICALLY (DURING CI)   !!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+
+# Root dir of the project
+SCRIPT_DIR=$(dirname "$0")
+PROJECT_ROOT=$(realpath "$SCRIPT_DIR/../..")
+I18N_CONFIG_FILE_PATH=$(realpath "$PROJECT_ROOT/.i18nrc")
+
+# Configure the current Git user (the GitHub actor)
+git config --local user.name "${GITHUB_ACTOR}"
+git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+# Stage downloaded files
+for source_path in $(jq -r '.translationSourcePaths[]' $I18N_CONFIG_FILE_PATH); do
+  git add $(dirname "$source_path")
+done
+
+if git diff-index --cached --quiet HEAD; then
+  # there are no changes to commit
+  echo "No translations updates"
+else
+  # Create a commit for staged files and push
+  git commit -m "${COMMIT_TITLE}"
+  git push -u origin ${BRANCH_NAME}
+
+  # Create a PR on the default branch (using GitHub CLI)
+  gh pr create --base ${BASE_REF} --head ${BRANCH_NAME} --fill
+fi
