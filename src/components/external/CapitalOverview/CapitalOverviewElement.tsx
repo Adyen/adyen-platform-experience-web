6e5f1444df821efed7240b711b50f4ff610bc1dd
germanm <german.mora@adyen.com>
2024-10-07 16:39:24 +0200

    Add http calls to get state

--- src/components/external/CapitalOverview/CapitalOverviewElement.tsx
+++ src/components/external/CapitalOverview/CapitalOverviewElement.tsx
@@ -2,7 +2,6 @@ import { _UIComponentProps } from '../../types';
 import UIElement from '../UIElement/UIElement';
 import { CapitalComponentStatus, CapitalOverviewProps } from './types';
 import { CapitalOverview } from './components/CapitalOverview/CapitalOverview';
-import type { SetupContext } from '../../../core/Auth';
 import { EMPTY_OBJECT } from '../../../utils';
 
 export class CapitalOverviewElement extends UIElement<CapitalOverviewProps> {
@@ -10,25 +9,32 @@ export class CapitalOverviewElement extends UIElement<CapitalOverviewProps> {
 
     constructor(props: _UIComponentProps<CapitalOverviewProps>) {
         super(props);
-        this.endpoints = props as any;
+
         this.componentToRender = this.componentToRender.bind(this);
     }
-
-    private readonly endpoints?: SetupContext['endpoints'] = undefined;
 
     public componentToRender = () => {
         return <CapitalOverview {...this.props} ref={(ref: UIElement<CapitalOverviewProps>) => void (this.componentRef = ref)} />;
     };
 
     public async getState(): Promise<CapitalComponentStatus> {
-        const { getGrants, getDynamicGrantOffersConfiguration } = this.endpoints ?? {};
+        const http = this.props.core.session.context.http;
 
-        const grants = await getGrants?.(EMPTY_OBJECT);
+        const { getGrants, getDynamicGrantOffersConfiguration } = /* this.props.core.session.context.endpoints || {} */ {
+            getGrants: async (options: any) =>
+                await http?.({ method: 'GET', loadingContext: this.props.core.loadingContext, path: '/capital/grants' }),
+            getDynamicGrantOffersConfiguration: async (options: any) =>
+                await http?.({ method: 'GET', loadingContext: this.props.core.loadingContext, path: '/capital/grantOffers/dynamic/configuration' }),
+        };
+
+        const grants = await getGrants(EMPTY_OBJECT);
         const config = await getDynamicGrantOffersConfiguration?.(EMPTY_OBJECT);
 
-        console.log(this.endpoints);
-        console.log('grants', grants);
-        console.log('config', config);
+        if (grants.data.length > 0 || (config && config.minAmount)) {
+            return 'OfferAvailable';
+        } else if (grants.data.length === 0 && (!config || !config.minAmount)) {
+            return 'NotQualified';
+        }
 
         return 'NotQualified';
     }
