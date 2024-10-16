f32d7f22c6edfe9f4df0eafda19d58caf834c68a
germanm <german.mora@adyen.com>
2024-10-16 12:01:15 +0200

    Create getErrorMessageUtil

---
+++ src/components/utils/capital/getCapitalErrorMessage.test.ts
@@ -0,0 +1,98 @@
+import { describe, it, expect, vi } from 'vitest';
+import { getCapitalErrorMessage, COMMON_CAPITAL_ERROR_MESSAGE } from './getCapitalErrorMessage'; // Update the path accordingly
+import AdyenPlatformExperienceError from '../../../core/Errors/AdyenPlatformExperienceError';
+import { UNDEFINED_ERROR } from '../getCommonErrorCode';
+import { ErrorTypes } from '../../../core/Http/utils';
+
+describe('getCapitalErrorMessage', () => {
+    it('should return UNDEFINED_ERROR when error is undefined', () => {
+        const result = getCapitalErrorMessage(undefined);
+        expect(result).toEqual(UNDEFINED_ERROR);
+    });
+
+    it('should return UNDEFINED_ERROR when errorCode is undefined', () => {
+        const error = new AdyenPlatformExperienceError(ErrorTypes.ERROR, 'requestId123', 'Test error message');
+        const result = getCapitalErrorMessage(error);
+
+        expect(result).toEqual({
+            ...UNDEFINED_ERROR,
+            refreshComponent: true,
+        });
+    });
+
+    it('should return UNDEFINED_ERROR with refreshComponent true when error.errorCode is undefined', () => {
+        const error = new AdyenPlatformExperienceError(ErrorTypes.ERROR, 'requestId123', 'Test error message');
+        error.errorCode = undefined;
+
+        const result = getCapitalErrorMessage(error);
+
+        expect(result).toEqual({
+            ...UNDEFINED_ERROR,
+            refreshComponent: true,
+        });
+    });
+
+    it('should return specific ErrorMessage when error.errorCode is "01_422"', () => {
+        const error = new AdyenPlatformExperienceError(ErrorTypes.ERROR, 'requestId123', 'Test error message', '01_422');
+
+        const mockOnContactSupport = vi.fn();
+
+        const result = getCapitalErrorMessage(error, mockOnContactSupport);
+
+        expect(result).toEqual({
+            title: COMMON_CAPITAL_ERROR_MESSAGE.somethingWentWrong,
+            message: [COMMON_CAPITAL_ERROR_MESSAGE.couldNotLoadOffers, COMMON_CAPITAL_ERROR_MESSAGE.contactSupport],
+            onContactSupport: mockOnContactSupport,
+        });
+    });
+
+    it('should return specific ErrorMessage when error.errorCode is "02_422"', () => {
+        const error = new AdyenPlatformExperienceError(ErrorTypes.ERROR, 'requestId123', 'Test error message', '02_422');
+
+        const mockOnContactSupport = vi.fn();
+
+        const result = getCapitalErrorMessage(error, mockOnContactSupport);
+
+        expect(result).toEqual({
+            title: 'capital.accountIsNotActive',
+            message: [COMMON_CAPITAL_ERROR_MESSAGE.couldNotLoadOffers, COMMON_CAPITAL_ERROR_MESSAGE.contactSupport],
+            onContactSupport: mockOnContactSupport,
+        });
+    });
+
+    it('should return specific ErrorMessage when error.errorCode is "03_422"', () => {
+        const error = new AdyenPlatformExperienceError(ErrorTypes.ERROR, 'requestId123', 'Test error message', '03_422');
+
+        const mockOnContactSupport = vi.fn();
+
+        const result = getCapitalErrorMessage(error, mockOnContactSupport);
+
+        expect(result).toEqual({
+            title: COMMON_CAPITAL_ERROR_MESSAGE.somethingWentWrong,
+            message: [COMMON_CAPITAL_ERROR_MESSAGE.couldNotLoadOffers, COMMON_CAPITAL_ERROR_MESSAGE.tryRefreshingThePage],
+            onContactSupport: mockOnContactSupport,
+        });
+    });
+
+    it('should return specific ErrorMessage when error.errorCode is "04_422"', () => {
+        const error = new AdyenPlatformExperienceError(ErrorTypes.ERROR, 'requestId123', 'Test error message', '04_422');
+
+        const mockOnContactSupport = vi.fn();
+
+        const result = getCapitalErrorMessage(error, mockOnContactSupport);
+
+        expect(result).toEqual({
+            title: COMMON_CAPITAL_ERROR_MESSAGE.somethingWentWrong,
+            message: ['capital.couldNotContinueWithTheOffer', COMMON_CAPITAL_ERROR_MESSAGE.contactSupport],
+            onContactSupport: mockOnContactSupport,
+        });
+    });
+
+    it('should return UNDEFINED_ERROR when error.errorCode is unknown', () => {
+        const error = new AdyenPlatformExperienceError(ErrorTypes.ERROR, 'requestId123', 'Test error message', 'unknown_code');
+
+        const result = getCapitalErrorMessage(error);
+
+        expect(result).toEqual(UNDEFINED_ERROR);
+    });
+});
---
+++ src/components/utils/capital/getCapitalErrorMessage.ts
@@ -0,0 +1,52 @@
+import AdyenPlatformExperienceError from '../../../core/Errors/AdyenPlatformExperienceError';
+import { ErrorMessage, getCommonErrorMessage, UNDEFINED_ERROR } from '../getCommonErrorCode';
+
+export const COMMON_CAPITAL_ERROR_MESSAGE = {
+    contactSupport: 'capital.pleaseContactSupportForHelp',
+    couldNotLoadOffers: 'capital.couldNotLoadFinancialOffers',
+    tryRefreshingThePage: 'capital.tryRefreshingThePageOrComeBackLater',
+    somethingWentWrong: 'capital.somethingWentWrong',
+} as const;
+
+export const getCapitalErrorMessage = (error: AdyenPlatformExperienceError | undefined, onContactSupport?: () => void): ErrorMessage => {
+    if (!error) return UNDEFINED_ERROR;
+    const commonError = getCommonErrorMessage(error, onContactSupport);
+    if (commonError) return commonError;
+    switch (error.errorCode) {
+        case undefined:
+            return {
+                ...UNDEFINED_ERROR,
+                refreshComponent: true,
+            };
+        case '01_422': {
+            return {
+                title: COMMON_CAPITAL_ERROR_MESSAGE.somethingWentWrong,
+                message: [COMMON_CAPITAL_ERROR_MESSAGE.couldNotLoadOffers, COMMON_CAPITAL_ERROR_MESSAGE.contactSupport],
+                onContactSupport,
+            };
+        }
+        case '02_422': {
+            return {
+                title: 'capital.accountIsNotActive',
+                message: [COMMON_CAPITAL_ERROR_MESSAGE.couldNotLoadOffers, COMMON_CAPITAL_ERROR_MESSAGE.contactSupport],
+                onContactSupport,
+            };
+        }
+        case '03_422': {
+            return {
+                title: COMMON_CAPITAL_ERROR_MESSAGE.somethingWentWrong,
+                message: [COMMON_CAPITAL_ERROR_MESSAGE.couldNotLoadOffers, COMMON_CAPITAL_ERROR_MESSAGE.tryRefreshingThePage],
+                onContactSupport,
+            };
+        }
+        case '04_422': {
+            return {
+                title: COMMON_CAPITAL_ERROR_MESSAGE.somethingWentWrong,
+                message: ['capital.couldNotContinueWithTheOffer', COMMON_CAPITAL_ERROR_MESSAGE.contactSupport],
+                onContactSupport,
+            };
+        }
+        default:
+            return UNDEFINED_ERROR;
+    }
+};
