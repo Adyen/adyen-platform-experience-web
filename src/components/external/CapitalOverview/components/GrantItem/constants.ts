bb77ea98df07c85323f94e07a13881a999eddaa3
isuert <isuert.mjalti@adyen.com>
2024-10-17 12:54:50 +0200

    Add expandable container in grant item

--- src/components/external/CapitalOverview/components/GrantItem/constants.ts
+++ src/components/external/CapitalOverview/components/GrantItem/constants.ts
@@ -1,5 +1,7 @@
 export const GRANT_ITEM_CLASS_NAMES = {
     base: 'adyen-pe-grant-item',
+    overview: 'adyen-pe-grant-item__overview',
+    details: 'adyen-pe-grant-item__details',
     statusContainer: 'adyen-pe-grant-item__status-container',
     textSecondary: 'adyen-pe-grant-item__text--secondary',
     progressBar: 'adyen-pe-grant-item__progress-bar',
