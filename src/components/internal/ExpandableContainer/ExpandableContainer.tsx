f474808113707eee4180e05135d3e6872ac532b3
isuert <isuert.mjalti@adyen.com>
2024-10-17 12:53:27 +0200

    Introduce expandable container

---
+++ src/components/internal/ExpandableContainer/ExpandableContainer.tsx
@@ -0,0 +1,38 @@
+import { ExpandableContainerProps } from './types';
+import { useState } from 'preact/hooks';
+import './ExpandableContainer.scss';
+import useCoreContext from '../../../core/Context/useCoreContext';
+import { FunctionalComponent } from 'preact';
+import { EXPANDABLE_CONTAINER_CLASS_NAMES } from './constants';
+import cx from 'classnames';
+import Button from '../Button/Button';
+import { ButtonVariant } from '../Button/types';
+import Icon from '../Icon';
+import uuid from '../../../utils/random/uuid';
+
+export const ExpandableContainer: FunctionalComponent<ExpandableContainerProps> = ({ className, children }) => {
+    const { i18n } = useCoreContext();
+    const [isOpen, setIsOpen] = useState(false);
+    const contentId = uuid();
+
+    return (
+        <div className={cx(EXPANDABLE_CONTAINER_CLASS_NAMES.base, className)} data-testid="expandable-container">
+            {isOpen && (
+                <div id={contentId} className={EXPANDABLE_CONTAINER_CLASS_NAMES.content}>
+                    {children}
+                </div>
+            )}
+            <Button
+                variant={ButtonVariant.TERTIARY_WITH_BACKGROUND}
+                condensed
+                fullWidth
+                onClick={() => setIsOpen(!isOpen)}
+                aria-controls={contentId}
+                aria-expanded={isOpen}
+                aria-label={i18n.get(isOpen ? 'expandableCard.collapse' : 'expandableCard.expand')}
+            >
+                <Icon name={isOpen ? 'chevron-up' : 'chevron-down'} className={EXPANDABLE_CONTAINER_CLASS_NAMES.toggleButtonIcon} />
+            </Button>
+        </div>
+    );
+};
