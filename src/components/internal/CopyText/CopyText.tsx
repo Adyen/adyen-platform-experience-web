49e9999db1684799102e4e22efb81867ed8c0db0
germanm <german.mora@adyen.com>
2024-10-31 16:26:52 +0100

    Add tooltip and copyText function to grant ID

--- src/components/internal/CopyText/CopyText.tsx
+++ src/components/internal/CopyText/CopyText.tsx
@@ -4,38 +4,50 @@ import { useCallback, useState } from 'preact/hooks';
 import { Tooltip } from '../Tooltip/Tooltip';
 import Copy from '../SVGIcons/Copy';
 import './CopyText.scss';
+import cx from 'classnames';
+import useCoreContext from '../../../core/Context/useCoreContext';
 
-const CopyText = ({ text }: { text: string }) => {
+const CopyText = ({ text, isHovered, label, type = 'Link' }: { text: string; isHovered?: boolean; label?: string; type?: 'Link' | 'Text' }) => {
     const [tooltipLabel, setTooltipLabel] = useState(text);
+    const { i18n } = useCoreContext();
 
     const onClick = useCallback(async () => {
         if (text) {
             try {
                 await navigator.clipboard.writeText(text);
-                setTooltipLabel('Copied');
+                setTooltipLabel(i18n.get('copied'));
             } catch (e) {
                 console.log(e);
             }
         }
-    }, [text]);
+    }, [i18n, text]);
 
     const resetTooltipLabel = useCallback(() => {
         setTooltipLabel(text);
     }, [setTooltipLabel, text]);
 
     return (
-        <Tooltip content={tooltipLabel}>
-            <Button
-                variant={ButtonVariant.TERTIARY}
-                className={'adyen-pe-copy-text'}
-                onClick={onClick}
-                onBlur={resetTooltipLabel}
-                onMouseLeaveCapture={resetTooltipLabel}
-            >
-                <span className={'adyen-pe-copy-text__information'}>{text}</span>
-                <Copy fill={'#0070f5'} />
-            </Button>
-        </Tooltip>
+        <Button
+            variant={ButtonVariant.TERTIARY}
+            className={'adyen-pe-copy-text'}
+            onClick={onClick}
+            onBlur={resetTooltipLabel}
+            onMouseLeaveCapture={resetTooltipLabel}
+        >
+            <Tooltip content={tooltipLabel} isContainerHovered={isHovered}>
+                <div className={'adyen-pe-copy-text__container'}>
+                    <span
+                        className={cx({
+                            ['adyen-pe-copy-text__information']: type === 'Link',
+                            ['adyen-pe-copy-text__text']: type === 'Text',
+                        })}
+                    >
+                        {label || text}
+                    </span>
+                    <Copy fill={type === 'Link' ? '#0070f5' : undefined} />
+                </div>
+            </Tooltip>
+        </Button>
     );
 };
 
