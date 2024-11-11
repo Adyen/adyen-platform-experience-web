ed18a24f880f302f2463d0b328a63d6cd83f7fb0
isuert <isuert.mjalti@adyen.com>
2024-11-11 11:45:37 +0100

    Adjust implementation to the new API contract

--- mocks/mock-server/setup.ts
+++ mocks/mock-server/setup.ts
@@ -79,6 +79,10 @@ export const setupMock = [
                     method: 'GET',
                     url: 'capital/grants/missingActions/signToS',
                 },
+                signToSActionDetails: {
+                    method: 'GET',
+                    url: 'capital/grants/missingActions/signToS',
+                },
             } satisfies Record<EndpointName, { method: HttpMethod; url: string }>,
         });
     }),
