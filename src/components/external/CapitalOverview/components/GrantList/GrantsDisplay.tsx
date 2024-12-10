fd631b01ce038edc3c1b73c2af776c820b1d583b
germanm <german.mora@adyen.com>
2024-12-10 15:27:25 +0100

    Hide tabs when grants length is > 2 but there are not active or inactive grants

--- src/components/external/CapitalOverview/components/GrantList/GrantsDisplay.tsx
+++ src/components/external/CapitalOverview/components/GrantList/GrantsDisplay.tsx
@@ -42,8 +42,9 @@ export const GrantsDisplay: FunctionalComponent<GrantsProps> = ({ grantList, hid
 
     const displayMode = useMemo<'SingleGrant' | 'Tabs' | undefined>(() => {
         if (grantList.length === 1) return 'SingleGrant';
-        if (grantList.length > 1) return 'Tabs';
-    }, [grantList.length]);
+        if (grantList.length > 1 && activeGrants.length && inactiveGrants.length) return 'Tabs';
+        return 'SingleGrant';
+    }, [activeGrants.length, grantList.length, inactiveGrants.length]);
 
     const showNewOfferButton = useMemo(() => {
         return newOfferAvailable && !activeGrants.some(grant => grant.status === 'Pending');
