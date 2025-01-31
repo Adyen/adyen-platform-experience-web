64eb0ef4f22cfe44edaf64939cdb7a35136047f5
elifd <elif.dogali@adyen.com>
2025-01-31 12:41:05 +0100

    Add translations for the summary legal text

--- src/components/external/CapitalOffer/components/CapitalOfferSummary/CapitalOfferSummaryLegalText.tsx
+++ src/components/external/CapitalOffer/components/CapitalOfferSummary/CapitalOfferSummaryLegalText.tsx
@@ -1,33 +1,40 @@
 import { useConfigContext } from '../../../../../core/ConfigContext';
+import useCoreContext from '../../../../../core/Context/useCoreContext';
 import AnchorButton from '../../../../internal/AnchorButton/AnchorButton';
-import Button from '../../../../internal/Button';
 import { ButtonVariant } from '../../../../internal/Button/types';
 import { AllowedLocations } from '../../../../internal/CapitalHeader/constants';
 import Card from '../../../../internal/Card/Card';
-import { TypographyElement, TypographyVariant } from '../../../../internal/Typography/types';
+import { Translation } from '../../../../internal/Translation';
+import { TypographyVariant } from '../../../../internal/Typography/types';
 import Typography from '../../../../internal/Typography/Typography';
-import { US_SUMMARY_TEXT_EMAIL, US_SUMMARY_TEXT_FIRST_PART, US_SUMMARY_TEXT_SECOND_PART, US_SUMMARY_TITLE } from './constants';
+import { SUMMARY_TEXT_EMAIL } from './constants';
 
 export const CapitalOfferSummaryLegalText = () => {
+    const { i18n } = useCoreContext();
     const region = useConfigContext()?.extraConfig?.legalEntity?.countryCode;
 
     return region === AllowedLocations.US ? (
         <Card filled noOutline>
-            <Typography variant={TypographyVariant.CAPTION} el={TypographyElement.DIV}>
-                {US_SUMMARY_TITLE}
+            <Typography variant={TypographyVariant.CAPTION} className={'adyen-pe-capital-offer-summary__legal-text--title'}>
+                {i18n.get('capital.summaryTitle')}
             </Typography>
-            <Typography variant={TypographyVariant.CAPTION} el={TypographyElement.PARAGRAPH} className={'adyen-pe-capital-offer-summary__info--text'}>
-                {US_SUMMARY_TEXT_FIRST_PART}
-            </Typography>
-            <AnchorButton
-                href={`mailto:${US_SUMMARY_TEXT_EMAIL}`}
-                variant={ButtonVariant.TERTIARY}
-                className={'adyen-pe-capital-offer-summary__info--email'}
-            >
-                {US_SUMMARY_TEXT_EMAIL}
-            </AnchorButton>
-            <Typography variant={TypographyVariant.CAPTION} el={TypographyElement.PARAGRAPH} className={'adyen-pe-capital-offer-summary__info--text'}>
-                {US_SUMMARY_TEXT_SECOND_PART}
+            <Typography variant={TypographyVariant.CAPTION} className={'adyen-pe-capital-offer-summary__legal-text--subtitle'}>
+                {
+                    <Translation
+                        translationKey={'capital.summaryParagraph'}
+                        fills={{
+                            email: (
+                                <AnchorButton
+                                    href={`mailto:${SUMMARY_TEXT_EMAIL}`}
+                                    variant={ButtonVariant.TERTIARY}
+                                    className={'adyen-pe-capital-offer-summary__info--email'}
+                                >
+                                    {SUMMARY_TEXT_EMAIL}
+                                </AnchorButton>
+                            ),
+                        }}
+                    />
+                }
             </Typography>
         </Card>
     ) : null;
