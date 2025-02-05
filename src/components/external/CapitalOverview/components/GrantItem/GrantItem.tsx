d26023a37fe3fbb4cd18b797fa5cfc85150f0080
glad <glad.chinda@adyen.com>
2025-02-05 18:34:32 +0100

    refactor: hoist grant item base class

--- src/components/external/CapitalOverview/components/GrantItem/GrantItem.tsx
+++ src/components/external/CapitalOverview/components/GrantItem/GrantItem.tsx
@@ -27,98 +27,98 @@ export const GrantItem: FunctionalComponent<GrantItemProps> = ({ grant }) => {
 
     const grantOverview = useMemo(
         () => (
-            <div className={GRANT_ITEM_CLASS_NAMES.base}>
-                <div className={GRANT_ITEM_CLASS_NAMES.cardContent}>
-                    <div className={GRANT_ITEM_CLASS_NAMES.statusContainer}>
-                        <Typography
-                            variant={TypographyVariant.CAPTION}
-                            className={cx({ [GRANT_ITEM_CLASS_NAMES.textSecondary]: grantConfig.isLabelColorSecondary })}
-                            testId={'grant-amount-label'}
-                        >
-                            {i18n.get(grantConfig.amountLabelKey)}
-                        </Typography>
-                        <div>
-                            {grant.status === 'Active' ? (
-                                <>
-                                    <Typography variant={TypographyVariant.CAPTION} el={TypographyElement.SPAN}>
-                                        {i18n.get('capital.termEnds')}
-                                    </Typography>
-                                    <Typography variant={TypographyVariant.CAPTION} stronger el={TypographyElement.SPAN}>
-                                        {dateFormat(grantConfig.repaymentPeriodEndDate, DATE_FORMAT_CAPITAL_OVERVIEW)}
-                                    </Typography>
-                                </>
-                            ) : grantConfig.statusKey ? (
-                                grantConfig.statusTooltipKey ? (
-                                    <Tooltip content={i18n.get(grantConfig.statusTooltipKey)}>
-                                        <div>
-                                            <Tag label={i18n.get(grantConfig.statusKey)} variant={grantConfig.statusTagVariant} />
-                                        </div>
-                                    </Tooltip>
-                                ) : (
-                                    <Tag label={i18n.get(grantConfig.statusKey)} variant={grantConfig.statusTagVariant} />
-                                )
-                            ) : null}
-                        </div>
-                    </div>
+            <div className={GRANT_ITEM_CLASS_NAMES.cardContent}>
+                <div className={GRANT_ITEM_CLASS_NAMES.statusContainer}>
                     <Typography
-                        variant={TypographyVariant.TITLE}
-                        medium
-                        className={cx({
-                            [GRANT_ITEM_CLASS_NAMES.textSecondary]: grantConfig.isAmountColorSecondary,
-                        })}
+                        variant={TypographyVariant.CAPTION}
+                        className={cx({ [GRANT_ITEM_CLASS_NAMES.textSecondary]: grantConfig.isLabelColorSecondary })}
+                        testId={'grant-amount-label'}
                     >
-                        {i18n.amount(grantConfig.amount.value, grantConfig.amount.currency)}
+                        {i18n.get(grantConfig.amountLabelKey)}
                     </Typography>
-                    {grantConfig.isProgressBarVisible && (
-                        <ProgressBar
-                            className={GRANT_ITEM_CLASS_NAMES.progressBar}
-                            value={grant.repaidTotalAmount.value}
-                            max={grant.totalAmount.value}
-                            labels={{ current: i18n.get('capital.repaid'), max: i18n.get('capital.remaining') }}
-                            tooltips={{
-                                remaining: `${i18n.amount(grant.remainingTotalAmount.value, grant.remainingTotalAmount.currency)} ${i18n
-                                    .get('capital.remaining')
-                                    ?.toLowerCase()}`,
-                                progress: `${i18n.amount(grant.repaidTotalAmount.value, grant.repaidTotalAmount.currency)} ${i18n
-                                    .get('capital.repaid')
-                                    ?.toLowerCase()}`,
-                            }}
-                        />
-                    )}
-                    {grantConfig.isGrantIdVisible ? (
-                        <div className={GRANT_ITEM_CLASS_NAMES.grantID}>
-                            <CopyText textToCopy={grant.id} buttonLabel={i18n.get('capital.grantID')} isHovered type={'Text'} />
-                        </div>
-                    ) : null}
-                    {grantConfig.hasAlerts && (
-                        <>
-                            {grant.missingActions && grant.missingActions.length ? (
-                                grant.missingActions.map(action => (
-                                    <GrantAction
-                                        key={action.type}
-                                        action={action}
-                                        className={GRANT_ITEM_CLASS_NAMES.alert}
-                                        offerExpiresAt={grant.offerExpiresAt}
-                                    />
-                                ))
+                    <div>
+                        {grant.status === 'Active' ? (
+                            <>
+                                <Typography variant={TypographyVariant.CAPTION} el={TypographyElement.SPAN}>
+                                    {i18n.get('capital.termEnds')}
+                                </Typography>
+                                <Typography variant={TypographyVariant.CAPTION} stronger el={TypographyElement.SPAN}>
+                                    {dateFormat(grantConfig.repaymentPeriodEndDate, DATE_FORMAT_CAPITAL_OVERVIEW)}
+                                </Typography>
+                            </>
+                        ) : grantConfig.statusKey ? (
+                            grantConfig.statusTooltipKey ? (
+                                <Tooltip content={i18n.get(grantConfig.statusTooltipKey)}>
+                                    <div>
+                                        <Tag label={i18n.get(grantConfig.statusKey)} variant={grantConfig.statusTagVariant} />
+                                    </div>
+                                </Tooltip>
                             ) : (
-                                <Alert
-                                    className={GRANT_ITEM_CLASS_NAMES.alert}
-                                    type={AlertTypeOption.HIGHLIGHT}
-                                    title={i18n.get('capital.weReceivedYourRequestAndWeAreWorkingOnItNowCheckBackSoon')}
-                                />
-                            )}
-                        </>
-                    )}
+                                <Tag label={i18n.get(grantConfig.statusKey)} variant={grantConfig.statusTagVariant} />
+                            )
+                        ) : null}
+                    </div>
                 </div>
+                <Typography
+                    variant={TypographyVariant.TITLE}
+                    medium
+                    className={cx({
+                        [GRANT_ITEM_CLASS_NAMES.textSecondary]: grantConfig.isAmountColorSecondary,
+                    })}
+                >
+                    {i18n.amount(grantConfig.amount.value, grantConfig.amount.currency)}
+                </Typography>
+                {grantConfig.isProgressBarVisible && (
+                    <ProgressBar
+                        className={GRANT_ITEM_CLASS_NAMES.progressBar}
+                        value={grant.repaidTotalAmount.value}
+                        max={grant.totalAmount.value}
+                        labels={{ current: i18n.get('capital.repaid'), max: i18n.get('capital.remaining') }}
+                        tooltips={{
+                            remaining: `${i18n.amount(grant.remainingTotalAmount.value, grant.remainingTotalAmount.currency)} ${i18n
+                                .get('capital.remaining')
+                                ?.toLowerCase()}`,
+                            progress: `${i18n.amount(grant.repaidTotalAmount.value, grant.repaidTotalAmount.currency)} ${i18n
+                                .get('capital.repaid')
+                                ?.toLowerCase()}`,
+                        }}
+                    />
+                )}
+                {grantConfig.isGrantIdVisible ? (
+                    <div className={GRANT_ITEM_CLASS_NAMES.grantID}>
+                        <CopyText textToCopy={grant.id} buttonLabel={i18n.get('capital.grantID')} isHovered type={'Text'} />
+                    </div>
+                ) : null}
+                {grantConfig.hasAlerts && (
+                    <>
+                        {grant.missingActions && grant.missingActions.length ? (
+                            grant.missingActions.map(action => (
+                                <GrantAction
+                                    key={action.type}
+                                    action={action}
+                                    className={GRANT_ITEM_CLASS_NAMES.alert}
+                                    offerExpiresAt={grant.offerExpiresAt}
+                                />
+                            ))
+                        ) : (
+                            <Alert
+                                className={GRANT_ITEM_CLASS_NAMES.alert}
+                                type={AlertTypeOption.HIGHLIGHT}
+                                title={i18n.get('capital.weReceivedYourRequestAndWeAreWorkingOnItNowCheckBackSoon')}
+                            />
+                        )}
+                    </>
+                )}
             </div>
         ),
         [i18n, dateFormat, grant, grantConfig]
     );
 
     return (
-        <ExpandableCard renderHeader={grantOverview} filled={grantConfig.isBackgroundFilled} inFlow>
-            {grantConfig.hasDetails && <GrantDetails grant={grant} />}
-        </ExpandableCard>
+        <div className={GRANT_ITEM_CLASS_NAMES.base}>
+            <ExpandableCard renderHeader={grantOverview} filled={grantConfig.isBackgroundFilled} inFlow>
+                {grantConfig.hasDetails && <GrantDetails grant={grant} />}
+            </ExpandableCard>
+        </div>
     );
 };
