6bbc364944b8f7a7397fcda17df7a91b4c599af5
germanm <german.mora@adyen.com>
2025-02-06 15:27:24 +0100

    Add paymentMethodDescription to transactionsOverview table

--- src/components/external/TransactionsOverview/components/utils.ts
+++ src/components/external/TransactionsOverview/components/utils.ts
@@ -6,6 +6,8 @@ const PAYMENT_METHODS = Object.freeze({
 });
 
 export function parsePaymentMethodType(paymentMethod: NonNullable<ITransaction['paymentMethod']>, format?: 'detail' | 'fourDigit') {
+    if (paymentMethod.paymentMethodDescription) return paymentMethod.paymentMethodDescription;
+
     if (paymentMethod.lastFourDigits) return format === 'detail' ? '•••• •••• •••• ' + paymentMethod.lastFourDigits : paymentMethod.lastFourDigits;
 
     return PAYMENT_METHODS[paymentMethod.type as keyof typeof PAYMENT_METHODS] || paymentMethod.type;
