b679bdd62f8a9c28ad04780f871f058ebf0f854e
isuert <isuert.mjalti@adyen.com>
2025-02-18 18:57:20 +0100

    Remove GrantDetailsViewHeader

--- src/components/external/CapitalOverview/components/GrantDetailsView/GrantDetailsView.tsx
+++ src/components/external/CapitalOverview/components/GrantDetailsView/GrantDetailsView.tsx
@@ -1,9 +1,13 @@
 import cx from 'classnames';
 import { FunctionalComponent, h } from 'preact';
-import { GrantDetailsViewHeader } from './GrantDetailsViewHeader';
 import type { TranslationKey } from '../../../../../translations';
 import type { GrantDetailsViewProps } from './types';
 import './GrantDetailsView.scss';
+import { Header } from '../../../../internal/Header';
+import Button from '../../../../internal/Button/Button';
+import { ButtonVariant } from '../../../../internal/Button/types';
+import Icon from '../../../../internal/Icon';
+import useCoreContext from '../../../../../core/Context/useCoreContext';
 
 export interface GrantDetailsViewComponentProps extends Pick<GrantDetailsViewProps, 'onDetailsClose'>, Pick<h.JSX.HTMLAttributes, 'className'> {
     headerTitleKey?: TranslationKey;
@@ -17,9 +21,21 @@ export const GrantDetailsView: FunctionalComponent<GrantDetailsViewComponentProp
     headerSubtitleKey,
     onDetailsClose,
 }) => {
+    const { i18n } = useCoreContext();
+
     return (
         <div className={cx('adyen-pe-grant-details-view', className)}>
-            <GrantDetailsViewHeader onDetailsClose={onDetailsClose} titleKey={headerTitleKey} subtitleKey={headerSubtitleKey} />
+            <Header titleKey={headerTitleKey} subtitleKey={headerSubtitleKey}>
+                <Button
+                    onClick={onDetailsClose}
+                    variant={ButtonVariant.TERTIARY}
+                    iconButton
+                    classNameModifiers={['circle']}
+                    aria-label={i18n.get('dismiss')}
+                >
+                    <Icon name="cross" />
+                </Button>
+            </Header>
             {children}
         </div>
     );
