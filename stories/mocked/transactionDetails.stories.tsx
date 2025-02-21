df9580d26a275461c2cc89fd975e147611fd8abc
germanm <german.mora@adyen.com>
2025-02-21 11:05:32 +0100

    Fix details

--- stories/mocked/transactionDetails.stories.tsx
+++ stories/mocked/transactionDetails.stories.tsx
@@ -13,6 +13,8 @@ export const Default: ElementStory<typeof TransactionDetails> = {
     },
 };
 
+const origin = process.env.VITE_PLAYGROUND_URL;
+
 export const CustomData: ElementStory<typeof TransactionDetails> = {
     name: 'Custom Data',
     args: {
@@ -21,13 +23,38 @@ export const CustomData: ElementStory<typeof TransactionDetails> = {
                 en_US: {
                     _store: 'Store',
                     _product: 'Product',
+                    _summary: 'Summary',
+                    _sendEmail: 'Email',
+                    _country: 'Country',
                 },
             },
         },
+        id: '1VVF0D5V3709DX6D',
         mockedApi: true,
         extraDetails: {
             _store: 'Sydney',
             _product: 'Coffee',
+            _summary: {
+                type: 'link',
+                value: 'Summary',
+                details: {
+                    href: `${origin}?path=/story/mocked-reports-overview--custom-columns&summary=${1}`,
+                },
+            },
+            _sendEmail: {
+                type: 'button',
+                value: 'Send email',
+                details: {
+                    action: () => console.log('Action'),
+                },
+            },
+            _country: {
+                type: 'icon',
+                value: '',
+                details: {
+                    src: `https://flagicons.lipis.dev/flags/4x3/es.svg`,
+                },
+            },
         },
     },
 };
