8adc3667e936d252e8140b4a8b4231f4c4c314bc
germanm <german.mora@adyen.com>
2025-03-31 10:10:36 +0200

    Remove unused variables

--- src/components/external/DisputesOverview/components/DisputesTable/constants.ts
+++ src/components/external/DisputesOverview/components/DisputesTable/constants.ts
@@ -1,5 +1,1 @@
-export const DISABLED_BUTTONS_TIMEOUT = 1000; // 1 second
-
-export const BASE_CLASS = 'adyen-pe-reports-table';
-export const DATE_TYPE_CLASS = `${BASE_CLASS}-date-report-type`;
-export const DATE_TYPE_DATE_SECTION_CLASS = `${DATE_TYPE_CLASS}--date`;
+export const BASE_CLASS = 'adyen-pe-disputes-table';
