16f5ff58ed80b17247dbd8d2ee24a5ebbe6debe0
glad <glad.chinda@adyen.com>
2025-04-02 16:34:50 +0200

    chore: added tests for file upload util

--- src/utils/file/upload.test.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @vitest-environment jsdom
- */
-import { describe, expect, test } from 'vitest';
-import { getUploadedFilesFromSource } from './upload';
-
-describe('getUploadedFilesFromSource', () => {
-    test('', () => {});
-});
--- src/utils/file/upload.ts
+++ src/utils/file/upload.ts
@@ -1,18 +1,40 @@
+/**
+ * A unified interface for file (uploaded files) sources, such as:
+ * - {@link HTMLInputElement `HTMLInputElement`} — with type of `file`
+ * - {@link ClipboardEvent `ClipboardEvent`}`.clipboardData`
+ * - {@link DragEvent `DragEvent`}`.dataTransfer`
+ * - {@link InputEvent `InputEvent`}`.dataTransfer`
+ */
 export interface UploadedFileSource {
-    files: DataTransfer['files'] | null;
-    items?: DataTransfer['items'];
+    files?: FileList | null;
+    items?: DataTransferItemList | null;
 }
 
+/**
+ * Extracts uploaded {@link File files} from the specified file source and returns them in an array.
+ * @param uploadedFileSource
+ */
 export const getUploadedFilesFromSource = <T extends UploadedFileSource>(uploadedFileSource?: T | null): File[] => {
-    const uploadedFiles: File[] = [];
+    const uploadedFiles = new Set<File>();
 
     if (uploadedFileSource?.items) {
+        // If there is a `DataTransferItemList` in the source (e.g. for data transfers),
+        // try extracting the uploaded files from the `items` list.
         for (const item of uploadedFileSource.items) {
             if (item.kind !== 'file') continue;
             const file = item.getAsFile();
-            if (file) uploadedFiles.push(file);
+            if (file) uploadedFiles.add(file);
         }
     }
 
-    return uploadedFiles.length === 0 ? Array.from(uploadedFileSource?.files ?? uploadedFiles) : uploadedFiles;
+    // If there are no files in the `DataTransferItemList` of the source,
+    // or there is no `DataTransferItemList` in the source (e.g. for input elements),
+    // try extracting the uploaded files from the `files` list of the source.
+
+    // prettier-ignore
+    return Array.from(
+        uploadedFiles.size === 0
+            ? new Set<File>(uploadedFileSource?.files ?? uploadedFiles)
+            : uploadedFiles
+    );
 };
