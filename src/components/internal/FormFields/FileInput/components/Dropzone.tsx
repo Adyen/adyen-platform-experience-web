300b937305cfe27e83037e3d75725f4084653cd0
glad <glad.chinda@adyen.com>
2025-04-08 17:19:43 +0200

    feat: rendering updates for file input error messages

--- src/components/internal/FormFields/FileInput/components/Dropzone.tsx
+++ src/components/internal/FormFields/FileInput/components/Dropzone.tsx
@@ -1,24 +1,29 @@
 import cx from 'classnames';
 import { RefObject } from 'preact';
-import { useCallback, useEffect, useMemo, useState } from 'preact/hooks';
+import { useCallback, useMemo, useState } from 'preact/hooks';
 import Typography from '../../../Typography/Typography';
 import useCoreContext from '../../../../../core/Context/useCoreContext';
 import useFocusVisibility from '../../../../../hooks/element/useFocusVisibility';
 import { BASE_CLASS, DEFAULT_FILE_TYPES, DEFAULT_MAX_FILE_SIZE, validationErrors } from '../constants';
 import { getUploadedFilesFromSource, uniqueId, UploadedFileSource } from '../../../../../utils';
 import { TypographyElement, TypographyVariant } from '../../../Typography/types';
+import { TranslationKey } from '../../../../../translations';
 import { DropzoneProps } from '../types';
 import Icon from '../../../Icon';
 import '../FileInput.scss';
 
 const classes = {
     dropzone: `${BASE_CLASS}__dropzone`,
+    dropzoneError: `${BASE_CLASS}__dropzone--error`,
     dropzoneHover: `${BASE_CLASS}__dropzone--hover`,
     label: `${BASE_CLASS}__label`,
     labelDefault: `${BASE_CLASS}__label-default-content`,
     labelIcon: `${BASE_CLASS}__label-icon`,
     labelText: `${BASE_CLASS}__label-text`,
     labelWithFocus: `${BASE_CLASS}__label--with-focus`,
+    error: `${BASE_CLASS}__error`,
+    errorIcon: `${BASE_CLASS}__error-icon`,
+    errorText: `${BASE_CLASS}__error-text`,
 };
 
 export function Dropzone({
@@ -36,8 +41,22 @@ export function Dropzone({
     const [inputError, setInputError] = useState('');
     const [zoneHover, setZoneHover] = useState(false);
 
-    const inputId = useMemo(() => id || uniqueId(), [id]);
+    const isInvalid = !!inputError;
     const inputName = name?.trim();
+    const inputId = useMemo(() => id || uniqueId(), [id]);
+
+    const inputValidationError = useMemo(() => {
+        switch (inputError) {
+            case validationErrors.MISSING_FILE:
+            case validationErrors.TOO_MANY_FILES:
+            case validationErrors.UNEXPECTED_FILE:
+            case validationErrors.VERY_LARGE_FILE:
+                // [TODO]: Define translations for validation error messages
+                return i18n.get('Upload at least one supporting document to continue' as TranslationKey);
+            default:
+                return inputError;
+        }
+    }, [i18n, inputError]);
 
     const handleDragOver = (event: DragEvent) => {
         const hasFiles = Array.from(event.dataTransfer?.types ?? []).some(type => type === 'Files');
@@ -62,28 +81,37 @@ export function Dropzone({
         updateFiles(event.target as HTMLInputElement);
     };
 
-    const handleInputBlur = (event: FocusEvent) => {
-        (event.target as HTMLInputElement)?.checkValidity();
+    const handleInputBlur = async (event: FocusEvent) => {
+        if (inputError) return;
+
+        if (required && (event.target as HTMLInputElement)?.validity.valueMissing) {
+            updateInputValidationError(validationErrors.MISSING_FILE);
+        }
     };
 
-    const handleInputInvalid = (event: Event) => {
-        const inputElement = event.target as HTMLInputElement;
-        // [TODO]: Trigger some error handler instead
-        console.log(inputElement.validationMessage);
-    };
+    const updateInputValidationError = useCallback((error: string) => {
+        const inputElement = (inputRef as RefObject<HTMLInputElement>).current;
+        inputElement?.setCustomValidity(error);
+        inputElement?.checkValidity();
+        setInputError(inputElement?.validationMessage ?? '');
+    }, []);
 
     const updateFiles = useCallback(
-        <T extends UploadedFileSource>(source?: T | null) => {
+        <T extends UploadedFileSource>(source?: T | null): void => {
             const uploadedFiles = getUploadedFilesFromSource(source);
 
             if (uploadedFiles.length > 1) {
-                return void setInputError(validationErrors.TOO_MANY_FILES);
+                return updateInputValidationError(validationErrors.TOO_MANY_FILES);
             }
 
             try {
                 const allowedFiles = uploadedFiles.filter(file => {
-                    if (!allowedFileTypes.includes(file.type)) throw validationErrors.UNEXPECTED_FILE;
-                    if (file.size > maxFileSize) throw validationErrors.VERY_LARGE_FILE;
+                    if (!allowedFileTypes.includes(file.type)) {
+                        throw validationErrors.UNEXPECTED_FILE;
+                    }
+                    if (file.size > maxFileSize) {
+                        throw validationErrors.VERY_LARGE_FILE;
+                    }
                     return true;
                 });
 
@@ -92,59 +120,70 @@ export function Dropzone({
                 switch (ex) {
                     case validationErrors.UNEXPECTED_FILE:
                     case validationErrors.VERY_LARGE_FILE:
-                        return void setInputError(ex);
+                        return updateInputValidationError(ex);
                 }
             }
         },
-        [allowedFileTypes, maxFileSize, setFiles]
+        [allowedFileTypes, maxFileSize, setFiles, updateInputValidationError]
     );
 
-    useEffect(() => {
-        const inputElement = (inputRef as RefObject<HTMLInputElement>).current;
-        inputElement?.setCustomValidity(inputError);
-    }, [inputError, inputRef]);
-
     return (
-        <div
-            role="region"
-            className={cx(classes.dropzone, { [classes.dropzoneHover]: zoneHover })}
-            onDragOver={disabled ? undefined : handleDragOver}
-            onDragLeave={disabled ? undefined : handleDragLeave}
-            onDrop={disabled ? undefined : handleDrop}
-        >
-            {/* Using the label element here to expose a user interaction surface for the file input element. */}
-            {/* The input element itself is visually hidden (not visible), but available to assistive technology. */}
-            <label className={cx(classes.label, { [classes.labelWithFocus]: hasVisibleFocus })} htmlFor={inputId}>
-                {children ?? (
-                    <div className={cx(classes.labelDefault)}>
-                        <Icon name="upload" className={classes.labelIcon} />
-                        <Typography
-                            /* Using the styles for the tertiary button here to have the same look and feel */
-                            className={cx(classes.labelText, 'adyen-pe-button', 'adyen-pe-button--tertiary')}
-                            el={TypographyElement.SPAN}
-                            variant={TypographyVariant.BODY}
-                        >
-                            {i18n.get('uploadFile.browse')}
-                        </Typography>
-                    </div>
-                )}
-            </label>
+        <>
+            <div
+                role="region"
+                className={cx(classes.dropzone, { [classes.dropzoneHover]: zoneHover, [classes.dropzoneError]: isInvalid })}
+                onDragOver={disabled ? undefined : handleDragOver}
+                onDragLeave={disabled ? undefined : handleDragLeave}
+                onDrop={disabled ? undefined : handleDrop}
+            >
+                {/* Using the label element here to expose a user interaction surface for the file input element. */}
+                {/* The input element itself is visually hidden (not visible), but available to assistive technology. */}
+                <label className={cx(classes.label, { [classes.labelWithFocus]: hasVisibleFocus })} htmlFor={inputId}>
+                    {children ?? (
+                        <div className={cx(classes.labelDefault)}>
+                            {
+                                // prettier-ignore
+                                isInvalid
+                                    ? <Icon name="warning-filled" className={classes.labelIcon} />
+                                    : <Icon name="upload" className={classes.labelIcon} />
+                            }
+                            <Typography
+                                /* Using the styles for the tertiary button here to have the same look and feel */
+                                className={cx(classes.labelText, 'adyen-pe-button', 'adyen-pe-button--tertiary')}
+                                el={TypographyElement.SPAN}
+                                variant={TypographyVariant.BODY}
+                                stronger
+                            >
+                                {i18n.get('uploadFile.browse')}
+                            </Typography>
+                        </div>
+                    )}
+                </label>
 
-            <input
-                type="file"
-                className="adyen-pe-visually-hidden"
-                id={inputId}
-                ref={inputRef}
-                name={inputName}
-                disabled={disabled}
-                required={required}
-                accept={String(allowedFileTypes)}
-                onBlur={handleInputBlur}
-                onChange={handleFileChange}
-                onInvalid={handleInputInvalid}
-                data-testId="dropzone-input"
-            />
-        </div>
+                <input
+                    type="file"
+                    className="adyen-pe-visually-hidden"
+                    id={inputId}
+                    ref={inputRef}
+                    name={inputName}
+                    disabled={disabled}
+                    required={required}
+                    accept={String(allowedFileTypes)}
+                    onBlur={handleInputBlur}
+                    onChange={handleFileChange}
+                    aria-invalid={isInvalid}
+                    data-testId="dropzone-input"
+                />
+            </div>
+            {isInvalid && (
+                <div className={classes.error}>
+                    <Icon name="cross-circle-fill" className={classes.errorIcon} />
+                    <Typography className={classes.errorText} el={TypographyElement.SPAN} variant={TypographyVariant.BODY}>
+                        {inputValidationError}
+                    </Typography>
+                </div>
+            )}
+        </>
     );
 }
 
--- src/components/internal/FormFields/FileInput/constants.ts
+++ src/components/internal/FormFields/FileInput/constants.ts
@@ -3,6 +3,7 @@ export const DEFAULT_FILE_TYPES = ['application/pdf', 'image/jpeg', 'image/jpg',
 export const DEFAULT_MAX_FILE_SIZE = 2097152; // 2MB
 
 export const validationErrors = {
+    MISSING_FILE: 'missing_file',
     TOO_MANY_FILES: 'too_many_files',
     UNEXPECTED_FILE: 'unexpected_file',
     VERY_LARGE_FILE: 'very_large_file',
