f92ed565e5d11d9fdd182d2b72aca889bbeed472
germanm <german.mora@adyen.com>
2025-04-29 11:13:03 +0200

    Fix IDisputeStatusGroup type

--- src/types/api/models/disputes.ts
+++ src/types/api/models/disputes.ts
@@ -1,8 +1,8 @@
 import { Schema } from '../utils';
-import { components, operations } from '../resources/DisputesResource';
+import { components } from '../resources/DisputesResource';
 
 export type IDispute = Schema<components, 'Dispute'>;
 export type IDisputeListItem = Schema<components, 'DisputeListItem'>;
 export type IDisputeDetail = Schema<components, 'DisputeDetailResponse'>;
-export type IDisputeStatusGroup = operations['getDisputeList']['parameters']['query']['statusGroup'];
+export type IDisputeStatusGroup = Schema<components, 'StatusGroup'>;
 export type IDisputeDefenseDocument = Schema<components, 'ApplicableDefenseDocument'>;
