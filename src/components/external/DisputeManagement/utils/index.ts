d5338ec22b77579c837912953621b04f9a2ed196
germanm <german.mora@adyen.com>
2025-05-21 11:57:35 +0200

    Remove string literal markup

--- src/components/external/DisputeManagement/utils/index.ts
+++ src/components/external/DisputeManagement/utils/index.ts
@@ -8,34 +8,42 @@ type TranslationConfigItem = {
     helpitems?: string[];
 };
 
-const getTranslationIfExists = (i18n: Localization['i18n'], prefix: string, key: string) => {
+const getTranslationIfExists = (i18n: Localization['i18n'], prefix: string, key: string): string | undefined => {
     const prefixedKey = `${prefix}.${key}`;
     return i18n.has(prefixedKey) ? i18n.get(prefixedKey) : undefined;
 };
 
-const getContent = (i18n: Localization['i18n'], config: Record<string, TranslationConfigItem>, configItemKey: string, translationPrefix: string) => {
+const getContent = (
+    i18n: Localization['i18n'],
+    config: Record<string, TranslationConfigItem>,
+    configItemKey: string,
+    translationPrefix: string
+): undefined | { title: string; description: string[] | undefined; secondaryDescriptionItems: string[] | undefined } => {
     const configItem = config[configItemKey];
     if (!configItem) return undefined;
 
-    const title = getTranslationIfExists(i18n, translationPrefix, configItem.title);
+    const t = (k: string) => getTranslationIfExists(i18n, 'disputes.defenseDocument', k);
+
+    const title = t(configItem.title);
 
     const descriptionKeys = configItem.help ? (Array.isArray(configItem.help) ? configItem.help : [configItem.help]) : undefined;
-    const primaryDescriptionItems = descriptionKeys?.map(key => getTranslationIfExists(i18n, translationPrefix, key)).filter(Boolean);
+    const primaryDescriptionItems: string[] | undefined = descriptionKeys
+        ?.map(key => getTranslationIfExists(i18n, translationPrefix, key))
+        .filter((k): k is string => k !== undefined);
+
+    const secondaryDescriptionItems: string[] = [];
 
     if (primaryDescriptionItems?.length && configItem.helpitems) {
-        const secondaryDescriptionItems = configItem.helpitems
-            .map(item => {
-                const translation = getTranslationIfExists(i18n, translationPrefix, item);
-                return translation ? `<li>${translation}</li>` : undefined;
-            })
-            .filter(Boolean);
-
-        primaryDescriptionItems.push(`<ul>${secondaryDescriptionItems}</ul>`);
+        configItem.helpitems.forEach(item => {
+            const translation = getTranslationIfExists(i18n, translationPrefix, item);
+            if (translation) secondaryDescriptionItems.push(translation);
+        });
     }
 
-    const description = primaryDescriptionItems?.map(item => `<p>${item}</p>`).join();
-
-    return { title, ...(description ? { description } : {}) };
+    return {
+        title: title || '',
+        ...{ description: primaryDescriptionItems || undefined, secondaryDescriptionItems: secondaryDescriptionItems || undefined },
+    };
 };
 
 export const getDefenseDocumentContent = (i18n: Localization['i18n'], defenseDocumentKey: string) => {
