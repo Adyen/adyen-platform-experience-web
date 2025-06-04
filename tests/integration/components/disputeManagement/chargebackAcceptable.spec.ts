796f61c54f7871755b40296edd95441d933a792a
germanm <german.mora@adyen.com>
2025-06-04 10:21:48 +0200

    Add integration tests for accept flow

--- tests/integration/components/disputeManagement/chargebackAcceptable.spec.ts
+++ tests/integration/components/disputeManagement/chargebackAcceptable.spec.ts
@@ -4,7 +4,7 @@ import { goToStory } from '../../../utils/utils';
 const STORY_ID = 'mocked-dispute-management--chargeback-acceptable';
 
 test.describe('Chargeback - Acceptable', () => {
-    test('should render acceptable chargeback ', async ({ page }) => {
+    test('should render button to accept and alert for defending the chargeback', async ({ page }) => {
         await goToStory(page, { id: STORY_ID });
         await expect(page.locator('.adyen-pe-tag--default', { hasText: 'Chargeback' })).toBeVisible();
         await expect(page.getByRole('alert')).toBeVisible();
@@ -18,11 +18,27 @@ test.describe('Chargeback - Acceptable', () => {
         await expect(page.getByRole('button', { name: 'Defend chargeback' })).not.toBeVisible();
     });
 
-    test('should contact support button', async ({ page }) => {
+    test('should render contact support button', async ({ page }) => {
         await goToStory(page, { id: STORY_ID, args: { onContactSupport: 'Enabled' } });
 
         await expect(page.getByRole('button', { name: 'Accept' })).toBeVisible();
         await expect(page.getByRole('button', { name: 'Contact support' })).toBeVisible();
         await expect(page.getByRole('button', { name: 'Defend chargeback' })).not.toBeVisible();
+    });
+
+    test('should complete accept flow', async ({ page }) => {
+        await goToStory(page, { id: STORY_ID, args: { onContactSupport: 'Enabled' } });
+
+        await page.getByRole('button', { name: 'Accept' }).click();
+        await expect(
+            page.getByText(
+                'By accepting, you agree that the disputed amount will not be returned to your account and the dispute will be marked as Lost.'
+            )
+        ).toBeVisible();
+        await page.getByText('I agree').click();
+        await page.getByRole('button', { name: 'Accept chargeback' }).click();
+
+        await expect(page.getByText('The dispute has been accepted')).toBeVisible();
+        await expect(page.getByRole('button', { name: 'Show dispute details' })).toBeVisible();
     });
 });
