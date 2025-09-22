8e15a3660646894316947176292cdcf01b909626
elifd <elif.dogali@adyen.com>
2025-09-22 14:28:54 +0200

    Send custom translations to Mixpanel

--- src/hooks/useAnalytics/useAnalytics.ts
+++ src/hooks/useAnalytics/useAnalytics.ts
@@ -1,4 +1,5 @@
 import { useCallback, useEffect } from 'preact/hooks';
+import useCoreContext from '../../core/Context/useCoreContext';
 import { usePushAnalyticEvent } from './usePushAnalyticEvent';
 import type { AnalyticsEventPayload, EventQueueItem, MixpanelProperty, UserEvents } from '../../core/Analytics/analytics/user-events';
 
@@ -21,6 +22,7 @@ export const convertToEmbeddedEvent = (eventQueueItem: EventQueueItem): Embedded
 
 export const useAnalytics = ({ userEvents }: UseAnalyticsProps) => {
     const sdkVersion = process.env.VITE_VERSION;
+    const { i18n } = useCoreContext();
 
     const pushAnalyticsEvent = usePushAnalyticEvent();
 
@@ -29,7 +31,7 @@ export const useAnalytics = ({ userEvents }: UseAnalyticsProps) => {
             sdkVersion,
             userAgent: navigator.userAgent,
         });
-    }, [sdkVersion, userEvents]);
+    }, [sdkVersion, userEvents, i18n]);
 
     const pushEvents = useCallback(
         (data: EventQueueItem) => {
@@ -42,8 +44,24 @@ export const useAnalytics = ({ userEvents }: UseAnalyticsProps) => {
     useEffect(() => {
         userEvents.subscribe(pushEvents);
 
+        const customizedLocale = Object.keys(i18n.customTranslations);
+        if (customizedLocale.length > 0) {
+            for (const locale of customizedLocale) {
+                const translations = i18n.customTranslations[locale]!;
+                const keys = Object.keys(translations!);
+                if (keys.length > 0) {
+                    userEvents.addEvent('Customized translation', {
+                        category: 'PIE',
+                        subCategory: 'PIE Component',
+                        locale: locale,
+                        keys: keys,
+                    });
+                }
+            }
+        }
+
         return () => {
             userEvents.unsubscribe(pushEvents);
         };
-    }, [userEvents, pushEvents]);
+    }, [userEvents, pushEvents, i18n]);
 };
