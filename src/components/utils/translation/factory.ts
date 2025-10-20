aaaa6c1ba518583d5cd8648e9f850049cb033043
glad <glad.chinda@adyen.com>
2025-10-20 15:44:23 +0200

    feat: add utils for dynamic translation factories

---
+++ src/components/utils/translation/factory.ts
@@ -0,0 +1,34 @@
+import Localization, { TranslationKey, TranslationOptions } from '../../../core/Localization';
+import { EMPTY_OBJECT } from '../../../utils';
+
+export interface KeyFactoryConfig {
+    prefix?: string;
+    suffix?: string;
+}
+
+export interface KeyFactoryFunction {
+    (value?: string): string | undefined;
+}
+
+export interface TranslationFallbackFunction {
+    (translationKey: string | undefined, value: string | undefined, options: TranslationOptions | undefined): string | undefined;
+}
+
+export const createKeyFactoryFromConfig = (config?: KeyFactoryConfig): KeyFactoryFunction => {
+    const { prefix = '', suffix = '' } = config ?? (EMPTY_OBJECT as NonNullable<typeof config>);
+    return (value?: string) => value ?? `${prefix}${value}${suffix}`;
+};
+
+export const createDynamicTranslationFactory =
+    (keyFactory: KeyFactoryFunction, translationFallback?: TranslationFallbackFunction) =>
+    (i18n: Localization['i18n'], value?: string, options?: TranslationOptions) => {
+        let translation: string | undefined = undefined;
+        const translationKey = keyFactory(value);
+
+        if (translationKey != undefined) {
+            translation = i18n.get(translationKey as TranslationKey, options);
+            translation = translation === translationKey ? undefined : translation;
+        }
+
+        return translation ?? translationFallback?.(translationKey, value, options);
+    };
