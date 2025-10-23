d4f583c9e8419090051dd10ae85f89e93be21413
elifd <elif.dogali@adyen.com>
2025-10-23 12:46:24 +0200

    Revert additional data to test changes

--- stories/utils/Container.tsx
+++ stories/utils/Container.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'preact/compat';
 import { StoryContext } from '@storybook/types';
 import { PreactRenderer } from '@storybook/preact';
-import { AdyenPlatformExperience, PayoutsOverview, ReportsOverview } from '../../src';
+import { AdyenPlatformExperience } from '../../src';
 import BaseElement from '../../src/components/external/BaseElement';
 import sessionRequest from './sessionRequest';
 import './styles.scss';
@@ -16,13 +16,9 @@ interface IContainer<T extends new (...args: any) => any> {
 
 export const Container = <T extends new (args: any) => any>({ component, componentConfiguration, context, locale }: IContainer<T>) => {
     const container = useRef(null);
-    const container2 = useRef(null);
-    const container3 = useRef(null);
 
     useEffect(() => {
         let Component: BaseElement<any>;
-        let Component2: BaseElement<any>;
-        let Component3: BaseElement<any>;
 
         void (async () => {
             const core = await AdyenPlatformExperience({
@@ -30,13 +26,6 @@ export const Container = <T extends new (args: any) => any>({ component, compone
                 balanceAccountId: context.args.balanceAccountId,
                 environment: 'test',
                 locale: locale || 'en-US',
-                translations: {
-                    en_US: {
-                        ['hey']: 'Test old translation',
-                        ['common.filters.types.date.label']: 'Test new translation',
-                        ['balanceAccounts.all']: 'Test new translation',
-                    },
-                },
                 onSessionCreate: async () => {
                     return await sessionRequest(context.args.session);
                 },
@@ -44,11 +33,7 @@ export const Container = <T extends new (args: any) => any>({ component, compone
             });
 
             Component = new component({ ...componentConfiguration, core });
-            Component2 = new PayoutsOverview({ core });
-            Component3 = new ReportsOverview({ core });
             Component.mount(container.current ?? '');
-            Component2.mount(container2.current ?? '');
-            Component3.mount(container3.current ?? '');
         })();
 
         return () => Component.unmount();
@@ -57,8 +42,6 @@ export const Container = <T extends new (args: any) => any>({ component, compone
     return (
         <>
             <div ref={container} id="component-root" className="component-wrapper" />
-            <div ref={container2} id="component-root2" className="component-wrapper" />
-            <div ref={container3} id="component-root3" className="component-wrapper" />
         </>
     );
 };
