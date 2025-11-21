35e221c2a47525c2c6946f7ffafe424b9cdc19f2
glad <glad.chinda@adyen.com>
2025-11-21 16:32:12 +0100

    refactor: address PR comments

--- src/components/internal/ToggleSwitch/types.ts
+++ src/components/internal/ToggleSwitch/types.ts
@@ -6,5 +6,5 @@ export interface ToggleSwitchProps extends AriaAttributes {
     name?: string;
     value?: string;
     checked?: boolean;
-    onChange?: (evt: h.JSX.TargetedEvent<HTMLInputElement>) => any;
+    onChange?: (evt: h.JSX.TargetedEvent<HTMLInputElement>) => void;
 }
