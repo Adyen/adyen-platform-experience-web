76a19dbcd65725912bd6319ec8699424cd2202ec
glad <glad.chinda@adyen.com>
2025-11-25 15:48:32 +0100

    refactor: address pr comments

--- src/components/internal/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/internal/ToggleSwitch/ToggleSwitch.tsx
@@ -1,22 +1,10 @@
 import { h } from 'preact';
 import { HTMLProps, PropsWithChildren } from 'preact/compat';
 import { useCallback, useMemo } from 'preact/hooks';
+import { classes } from './constants';
 import cx from 'classnames';
 import Icon from '../Icon';
 import './ToggleSwitch.scss';
-
-const BASE_CLASS = 'adyen-pe-toggle-switch';
-
-const classes = {
-    root: BASE_CLASS,
-    disabled: BASE_CLASS + '--disabled',
-    readonly: BASE_CLASS + '--readonly',
-    label: BASE_CLASS + '__label',
-    labelBefore: BASE_CLASS + '__label--before',
-    mark: BASE_CLASS + '__mark',
-    slider: BASE_CLASS + '__slider',
-    switch: BASE_CLASS + '__switch',
-};
 
 export interface ToggleSwitchProps extends PropsWithChildren<Omit<HTMLProps<HTMLInputElement>, 'readonly'>> {
     labelBeforeSwitch?: boolean;
