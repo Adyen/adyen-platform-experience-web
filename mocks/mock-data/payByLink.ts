231fe8bcf1f91b3e5408206a660fb9e704401547
samuelsa <samuel.sarabia@adyen.com>
2025-12-23 11:06:30 +0100

    fix unit tests

--- mocks/mock-data/payByLink.ts
+++ mocks/mock-data/payByLink.ts
@@ -1,18 +1,18 @@
 export const STORES = [
     {
         description: 'Main Store - New York',
-        storeCode: 'New York Store',
-        storeId: 'NY001',
+        storeCode: 'NY001',
+        storeId: 'STORE_NY_001',
     },
     {
         description: 'Main Store - London',
-        storeCode: 'London Store',
-        storeId: 'LD001',
+        storeCode: 'LN001',
+        storeId: 'STORE_LON_001',
     },
     {
         description: 'Main Store - Amsterdam',
-        storeCode: 'Amsterdam Store',
-        storeId: 'AM001',
+        storeCode: 'AM001',
+        storeId: 'STORE_AMS_001',
     },
 ];
 
@@ -197,17 +197,17 @@ const BASE_PAY_BY_LINK_CONFIGURATION = {
 const { linkValidity, deliveryAddress, ...configWithoutLinkValidityAndAddress } = BASE_PAY_BY_LINK_CONFIGURATION;
 
 export const PAY_BY_LINK_CONFIGURATION = {
-    NY001: BASE_PAY_BY_LINK_CONFIGURATION,
-    LD001: configWithoutLinkValidityAndAddress,
-    AM001: BASE_PAY_BY_LINK_CONFIGURATION,
+    STORE_NY_001: BASE_PAY_BY_LINK_CONFIGURATION,
+    STORE_LON_001: configWithoutLinkValidityAndAddress,
+    STORE_AMS_001: BASE_PAY_BY_LINK_CONFIGURATION,
 };
 
 export const PAY_BY_LINK_SETTINGS = {
-    NY001: {
+    STORE_NY_001: {
         termsOfServiceUrl: 'https://example.com/terms-and-conditions',
     },
-    LD001: { termsOfServiceUrl: 'https://example.com/terms-and-conditions' },
-    AM001: {},
+    STORE_LON_001: { termsOfServiceUrl: 'https://example.com/terms-and-conditions' },
+    STORE_AMS_001: {},
 };
 
 export const STORE_THEME = {
