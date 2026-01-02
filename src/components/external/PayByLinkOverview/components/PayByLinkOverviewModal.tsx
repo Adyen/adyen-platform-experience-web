d7be68c789374c3427b54365ff26f04712a63309
samuelsa <samuel.sarabia@adyen.com>
2026-01-02 09:59:49 +0100

    remove unnecessary dependency

--- src/components/external/PayByLinkOverview/components/PayByLinkOverviewModal.tsx
+++ src/components/external/PayByLinkOverview/components/PayByLinkOverviewModal.tsx
@@ -39,7 +39,7 @@ export const PayByLinkOverviewModal = ({
             refreshPaymentLinkList?.();
             setHasToRefresh(false);
         }
-    }, [onCloseModal, hasToRefresh, isModalVisible, refreshPaymentLinkList]);
+    }, [onCloseModal, hasToRefresh, refreshPaymentLinkList]);
 
     useEffect(() => {
         if (isModalVisible) {
