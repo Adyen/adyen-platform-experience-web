17c6566b3d60d5af4d6504bf19041b584b7f9986
elifd <elif.dogali@adyen.com>
2026-01-08 10:04:52 +0100

    Fix conflicts

--- src/components/external/PaymentLinksOverview/components/PaymentLinksOverviewContainer.tsx
+++ src/components/external/PaymentLinksOverview/components/PaymentLinksOverviewContainer.tsx
@@ -5,13 +5,14 @@ import './PaymentLinksOverview.scss';
 
 function PaymentLinksOverviewContainer({ ...props }: ExternalUIComponentProps<PaymentLinksOverviewComponentProps>) {
     //TODO: Add error case
-    const { filters, isFetching, stores, filterError, storeError } = usePaymentLinkFilters(props?.storeIds);
+    const { filters, isFetching, stores, filterError, allStores, storeError } = usePaymentLinkFilters(props?.storeIds);
 
     return (
         <PaymentLinksOverview
             {...props}
             filterParams={filters}
             stores={stores}
+            allStores={allStores}
             isFiltersLoading={isFetching}
             filterError={filterError}
             storeError={storeError}
