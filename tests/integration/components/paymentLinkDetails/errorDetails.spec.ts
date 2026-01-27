c84260381c2ab5dd0e2f4502b0c5159d5e8444a6
isuert <isuert.mjalti@adyen.com>
2026-01-27 17:07:25 +0100

    Review

--- tests/integration/components/paymentLinkDetails/errorDetails.spec.ts
+++ tests/integration/components/paymentLinkDetails/errorDetails.spec.ts
@@ -10,7 +10,7 @@ test.describe('Error - Details', () => {
     });
 
     test('should render details error', async ({ page }) => {
-        await expect(page.getByText('Payment link details')).toBeVisible();
+        await expect(page.getByText('Payment link details', { exact: true })).toBeVisible();
         await expect(page.getByText('Something went wrong.')).toBeVisible();
         await expect(page.getByText("We couldn't load the payment link details.")).toBeVisible();
         await expect(page.getByText('Try refreshing the page or come back later.')).toBeVisible();
