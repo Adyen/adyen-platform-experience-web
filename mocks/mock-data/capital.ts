c2f45651d772174956d91b238e3a91915243bf93
isuert <isuert.mjalti@adyen.com>
2026-02-25 16:18:28 +0100

    Mock onboarding-related endpoints

--- mocks/mock-data/capital.ts
+++ mocks/mock-data/capital.ts
@@ -1,4 +1,11 @@
-import { IDynamicOffersConfig, IGBCapitalFundsCollection, IGrant, INLCapitalFundsCollection, IUSCapitalFundsCollection } from '../../src';
+import {
+    IDynamicOffersConfig,
+    IGBCapitalFundsCollection,
+    IGrant,
+    INLCapitalFundsCollection,
+    IOnboardingConfiguration,
+    IUSCapitalFundsCollection,
+} from '../../src';
 
 export const DYNAMIC_CAPITAL_OFFER = {
     minAmount: {
@@ -247,4 +254,9 @@ export const SIGN_TOS_ACTION_DETAILS = {
 
 export const ANACREDIT_ACTION_DETAILS = {
     url: 'https://www.adyen.com/capital',
+};
+
+export const ONBOARDING_CONFIGURATION: IOnboardingConfiguration = {
+    token: 'test-onboarding-token',
+    legalEntityId: 'test-legal-entity-id',
 };
