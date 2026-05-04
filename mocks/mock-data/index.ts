57332232a65835fbd83d21ae6c25aa197ac020c6
germanm <german.mora@adyen.com>
2026-05-04 11:20:49 +0200

    Remove unused import

--- mocks/mock-data/index.ts
+++ mocks/mock-data/index.ts
@@ -2,6 +2,4 @@ export * from './balanceAccounts';
 export * from './capital';
 export * from './disputes';
 export * from './payByLink';
-export * from '../../packages/domains/payouts/mocks/mock-data/payouts';
-export * from '../../packages/domains/reports/mocks/mock-data/reports';
 export * from './transactions';
