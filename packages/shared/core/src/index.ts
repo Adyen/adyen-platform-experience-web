5e803120bcc0aa3bcafb7ee1d78d0cb344be9dfb
Samuel Sarabia <samuel.sarabia@adyen.com>
2026-05-12 10:55:35 +0200

    Adapt Core for Vue extension (#1139)

--- packages/shared/core/src/index.ts
+++ packages/shared/core/src/index.ts
@@ -69,4 +69,7 @@ export {
     SERVER_SIDE_INITIALIZATION_WARNING,
 } from './runtime';
 
-export type { CoreOptions, DevEnvironment, onErrorHandler, AnalyticsConfig } from './types';
+export type { ResolvedEnvironment, CoreOptions, DevEnvironment, onErrorHandler, AnalyticsConfig } from './types';
+
+export { Core } from './Core';
+export type { CdnFetcher, ManagedElement } from './Core';
