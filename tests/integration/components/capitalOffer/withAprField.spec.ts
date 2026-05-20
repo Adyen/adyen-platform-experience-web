9c42262503e1eb6bbe5017077d3b89b15cd33ffd
isuert <isuert.mjalti@adyen.com>
2026-05-20 17:10:34 +0200

    Update integration tests

--- tests/integration/components/capitalOffer/withAprField.spec.ts
+++ tests/integration/components/capitalOffer/withAprField.spec.ts
@@ -9,7 +9,7 @@ test.describe('With APR field', () => {
     });
 
     test('should render Annual percentage rate field', async ({ page }) => {
-        await page.getByRole('button', { name: 'Review offer' }).click();
+        await page.getByRole('button', { name: 'Review request' }).click();
         await expect(page.getByText('Annual percentage rate')).toBeVisible();
         await expect(page.getByText('20%')).toBeVisible();
     });
