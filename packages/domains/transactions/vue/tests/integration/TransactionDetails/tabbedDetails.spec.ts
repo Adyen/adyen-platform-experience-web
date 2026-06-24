456be1e6d4816c7ef23bf50b3f0dfb69a5da8521
glad <glad.chinda@adyen.com>
2026-06-24 11:46:24 +0200

    chore: vue integration tests for TransactionDetails component

---
+++ packages/domains/transactions/vue/tests/integration/TransactionDetails/tabbedDetails.spec.ts
@@ -0,0 +1,182 @@
+import type { Page } from '@playwright/test';
+import { test, expect } from '@integration-components/testing/fixtures/eventDispatcher/events';
+import { expectAnalyticsEvents, goToStory } from '@integration-components/testing/playwright/utils';
+import { sharedAnalyticsEventProperties } from '../../../../fixtures/constants/TransactionDetails';
+
+const STORY_ID = 'mocked-transactions-transaction-details--tabbed-details';
+
+test.describe('Tabbed details', () => {
+    const expectSamePaymentStatusBoxRendering = async (page: Page) => {
+        await expect(page.getByText('Payment', { exact: true }).first()).toBeVisible();
+        await expect(page.getByText('Partially refunded', { exact: true })).toBeVisible();
+
+        // [TODO]: Address amount formatting discrepancy (use currency code instead of symbol)
+        // Using first here to prevent clashes with other amounts displayed on page
+        // await expect(page.getByText('607.50 EUR', { exact: true }).first()).toBeVisible();
+        await expect(page.getByText('€607.50', { exact: true }).first()).toBeVisible();
+        await expect(page.getByText('•••• •••• •••• 1945', { exact: true })).toBeVisible();
+
+        // [TODO]: Address wrong timezone used for date formatting (use balance account timezone)
+        // await expect(page.getByText('Monday, August 29, 2022 at 09:47 AM GMT-3', { exact: true })).toBeVisible();
+        await expect(page.getByText('Monday, August 29, 2022 at 12:47 PM GMT+0', { exact: true })).toBeVisible();
+    };
+
+    const expectBeforePaymentRefundDetailsRendering = async (page: Page) => {
+        await expect(page.getByText('You already refunded €473.75', { exact: true })).toBeVisible();
+        await expect(page.getByRole('status')).toHaveCount(1);
+        await expect(page.getByRole('alert')).toHaveCount(0);
+        await expect(page.getByRole('button', { name: 'Refund payment', exact: true, disabled: false })).toBeVisible();
+    };
+
+    const expectSamePaymentSummaryRendering = async (page: Page) => {
+        await expect(page.getByText('Original amount', { exact: true })).toBeVisible();
+        await expect(page.getByText('642.00 EUR', { exact: true })).toBeVisible();
+
+        for (const labelText of ['Tip amount', 'Surcharge amount']) {
+            const labels = page.getByText(labelText, { exact: true });
+            const allLabels = await labels.all();
+
+            await expect(labels).toHaveCount(2);
+
+            for (const label of allLabels) {
+                await expect(label).toBeVisible();
+            }
+        }
+
+        await expect(page.getByText('1.50 EUR', { exact: true })).toBeVisible();
+        await expect(page.getByText('- 1.50 EUR', { exact: true })).toBeVisible();
+
+        await expect(page.getByText('15.00 EUR', { exact: true })).toBeVisible();
+        await expect(page.getByText('- 15.00 EUR', { exact: true })).toBeVisible();
+
+        await expect(page.getByText('Gross amount', { exact: true })).toBeVisible();
+        await expect(page.getByText('658.50 EUR', { exact: true })).toBeVisible();
+
+        await expect(page.getByText('Fees', { exact: true })).toBeVisible();
+        await expect(page.getByText('- 9.50 EUR', { exact: true })).toBeVisible();
+
+        await expect(page.getByText('Split payment', { exact: true })).toBeVisible();
+        await expect(page.getByText('- 25.00 EUR', { exact: true })).toBeVisible();
+
+        await expect(page.getByText('Net amount', { exact: true })).toBeVisible();
+        await expect(page.getByText('607.50 EUR', { exact: true }).first()).toBeVisible();
+
+        // [TODO]: Address amount formatting discrepancy (use currency code instead of symbol)
+        // await expect(page.getByText('607.50 EUR', { exact: true })).toHaveCount(2);
+        await expect(page.getByText('607.50 EUR', { exact: true })).toHaveCount(1);
+    };
+
+    const expectSamePaymentDetailsRendering = async (page: Page) => {
+        await expect(page.getByText('Account', { exact: true })).toBeVisible();
+        await expect(page.getByText('S. Hopper - Main Account', { exact: true })).toBeVisible();
+
+        await expect(page.getByText('Reference ID', { exact: true })).toBeVisible();
+        await expect(page.getByText('4B7N9Q2Y6R1W5M8T', { exact: true })).toBeVisible();
+
+        await expect(page.getByText('Merchant reference', { exact: true })).toBeVisible();
+        await expect(page.getByText('TX-F9X2V8L7P1K6W', { exact: true })).toBeVisible();
+
+        await expect(page.getByText('PSP reference', { exact: true })).toBeVisible();
+        await expect(page.getByText('PSP0000000000990', { exact: true })).toBeVisible();
+
+        await expect(page.getByRole('button', { name: /^Copy/, disabled: false })).toHaveCount(3);
+    };
+
+    const expectSamePaymentTimelineRendering = async (page: Page) => {
+        const expectTimelineItems = async (count: number) => {
+            // [TODO]: Address wrong timezone used for date formatting (use balance account timezone)
+            // for (const text of ['Aug 29, 2022, 09:47', 'Amount', 'Status']) {
+            for (const text of ['Aug 29, 2022, 12:47', 'Amount', 'Status']) {
+                const elems = page.getByText(text, { exact: true });
+                const allElems = await elems.all();
+
+                await expect(elems).toHaveCount(count);
+
+                for (const label of allElems) {
+                    await expect(label).toBeVisible();
+                }
+            }
+        };
+
+        await expectTimelineItems(2);
+
+        const showMoreButton = page.getByRole('button', { name: 'Show 1 more', exact: true, disabled: false });
+        const showLessButton = page.getByRole('button', { name: 'Show less', exact: true, disabled: false });
+
+        await expect(showMoreButton).toBeVisible();
+        await expect(showLessButton).toBeHidden();
+
+        await showMoreButton.click();
+        await expect(showMoreButton).toBeHidden();
+        await expect(showLessButton).toBeVisible();
+
+        await expectTimelineItems(3);
+
+        await showLessButton.click();
+        await expect(showMoreButton).toBeVisible();
+        await expect(showLessButton).toBeHidden();
+
+        await expectTimelineItems(2);
+    };
+
+    const navigateToTab = async (page: Page, name: 'Details' | 'Summary' | 'Timeline') => {
+        const navigatedToTab = expect(page.getByRole('tab', { name, exact: true, selected: true })).toBeVisible();
+        await expect(page.getByRole('tab', { name, exact: true, selected: false })).toBeVisible();
+        await page.getByRole('tab', { name, exact: true }).click();
+        await navigatedToTab;
+    };
+
+    test.beforeEach(async ({ page, analyticsEvents }) => {
+        await goToStory(page, { id: STORY_ID });
+        await expectAnalyticsEvents(analyticsEvents, [['Landed on page', sharedAnalyticsEventProperties]]);
+    });
+
+    test('should render navigation tabs', async ({ page }) => {
+        await expect(page.getByRole('tab', { name: 'Summary', exact: true })).toBeVisible();
+        await expect(page.getByRole('tab', { name: 'Details', exact: true })).toBeVisible();
+        await expect(page.getByRole('tab', { name: 'Timeline', exact: true })).toBeVisible();
+        await expect(page.getByRole('tab')).toHaveCount(3);
+    });
+
+    test('should initially render the "Summary" tab', async ({ page }) => {
+        await expectSamePaymentStatusBoxRendering(page);
+        await expectSamePaymentSummaryRendering(page);
+        await expectBeforePaymentRefundDetailsRendering(page);
+    });
+
+    test('should render the "Details" tab', async ({ page }) => {
+        await navigateToTab(page, 'Details');
+        await expectSamePaymentStatusBoxRendering(page);
+        await expectSamePaymentDetailsRendering(page);
+        await expectBeforePaymentRefundDetailsRendering(page);
+    });
+
+    test('should render the "Timeline" tab', async ({ page }) => {
+        // [TODO]: Address inconsistent timeline items rendering (should initially render at most 2 items)
+        test.fixme(true, 'Inconsistent transaction timeline items rendering');
+
+        await navigateToTab(page, 'Timeline');
+        await expectSamePaymentStatusBoxRendering(page);
+        await expectSamePaymentTimelineRendering(page);
+        await expectBeforePaymentRefundDetailsRendering(page);
+    });
+
+    test('should render the "Summary" tab', async ({ page }) => {
+        await navigateToTab(page, 'Details');
+        await navigateToTab(page, 'Summary');
+        await expectSamePaymentStatusBoxRendering(page);
+        await expectSamePaymentSummaryRendering(page);
+        await expectBeforePaymentRefundDetailsRendering(page);
+    });
+
+    test('should return to "Summary" tab from refund view', async ({ page }) => {
+        await navigateToTab(page, 'Details');
+        await page.getByRole('button', { name: 'Refund payment', exact: true, disabled: false }).click();
+        await page.getByRole('button', { name: 'Go back', exact: true, disabled: false }).click();
+
+        // Back to payment summary (same)
+        await expectSamePaymentStatusBoxRendering(page);
+        await expectSamePaymentSummaryRendering(page);
+        await expectBeforePaymentRefundDetailsRendering(page);
+    });
+});
