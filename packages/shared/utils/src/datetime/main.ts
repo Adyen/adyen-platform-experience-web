ca9697052cb3d0581e2749be1c5351db973663e6
Germán Mora <46287932+german664@users.noreply.github.com>
2026-07-14 12:08:17 +0200

    Update packages/shared/utils/src/datetime/main.ts
    Co-authored-by: Glad Chinda <117656861+glad-adyen@users.noreply.github.com>

--- packages/shared/utils/src/datetime/main.ts
+++ packages/shared/utils/src/datetime/main.ts
@@ -3,8 +3,8 @@ import type { DateFunction, DateTimeComponents } from './types';
 
 const MINUTE_IN_MS = 60 * 1000;
 const HOUR_IN_MS = 60 * MINUTE_IN_MS;
-export const DAY_MS = 24 * HOUR_IN_MS;
-const WEEK_IN_MS = 7 * DAY_MS;
+export const DAY_IN_MS = 24 * HOUR_IN_MS;
+const WEEK_IN_MS = 7 * DAY_IN_MS;
 
 export const parseDate = ((...args) => {
     const [value] = args;
@@ -17,15 +17,16 @@ export const parseDate = ((...args) => {
               : new Date((isString(value) || Number.isFinite(value) ? value : undefined)!);
 
     const timestamp = date.getTime();
-export const DAY_IN_MS = 24 * HOUR_IN_MS;
-const WEEK_IN_MS = 7 * DAY_IN_MS;
+    return Number.isFinite(timestamp) ? timestamp : undefined;
+}) as DateFunction<number | undefined>;
 
-export const parseDate = ((...args) => {
-const [value] = args;
-@@ -27,7 +27,7 @@ export const transformToMS = (unit: string, value: number): number => {
-case 'minute':
-return value * MINUTE_IN_MS;
-case 'day':
+export const transformToMS = (unit: string, value: number): number => {
+    switch (unit) {
+        case 'hour':
+            return value * HOUR_IN_MS;
+        case 'minute':
+            return value * MINUTE_IN_MS;
+        case 'day':
             return value * DAY_IN_MS;
         case 'week':
             return value * WEEK_IN_MS;
