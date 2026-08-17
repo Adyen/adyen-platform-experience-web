fa45793079301d77bf26ab42330de0d2530f5201
samuelsa <samuel.sarabia@adyen.com>
2026-08-17 14:47:12 +0200

    chore: Update bento and remove first/last page buttons

--- packages/domains/reports/vue/src/ReportsOverview/components/ReportsTable.vue
+++ packages/domains/reports/vue/src/ReportsOverview/components/ReportsTable.vue
@@ -207,6 +207,7 @@ const paginationProps = computed(() => {
         hasNext: props.hasNext ?? false,
         hasPrevious: props.hasPrevious ?? false,
         hidePageSize: !props.limitOptions || props.limitOptions.length <= 1,
+        hideFirstLastPageButtons: true,
     };
 });
 
