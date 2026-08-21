167bbe0f6599e3c4aa7291135ef230654a96ad09
germanm <german.mora@adyen.com>
2026-08-21 10:28:23 +0200

    Address comments

--- packages/domains/payByLink/vue/src/PaymentLinkSettings/components/SettingsActionButtons.vue
+++ packages/domains/payByLink/vue/src/PaymentLinkSettings/components/SettingsActionButtons.vue
@@ -5,7 +5,6 @@ import { useCoreContext } from '@integration-components/core/vue';
 import { usePaymentLinkSettingsContext } from '../composables/context';
 import { useSettingsPermission } from '../composables/useSettingsPermission';
 import { MenuItem } from '../constants';
-import { containerQueries, useResponsiveContainer } from '@integration-components/composables-vue';
 
 const props = defineProps<{
     navigateBack?: () => void;
@@ -15,7 +14,6 @@ const props = defineProps<{
 const { i18n } = useCoreContext();
 const { activeMenuItem, onSave, isSaving, isLoadingContent, isLoadingStores, isSaveSuccess } = usePaymentLinkSettingsContext();
 const { themeEnabled, termsAndConditionsEnabled } = useSettingsPermission();
-const isSmContainer = useResponsiveContainer(containerQueries.down.xs);
 
 const isLoading = computed(() => isLoadingContent.value || isLoadingStores.value);
 
@@ -48,6 +46,6 @@ const actionButtons = computed<BentoButtonActionsList>(() => {
 
 <template>
     <div class="adyen-pe-payment-link-settings__cta-container">
-        <BentoButtonActions :layout="isSmContainer ? 'vertical-stack' : 'buttons-end'" :actions="actionButtons" />
+        <BentoButtonActions layout="buttons-end" :actions="actionButtons" />
     </div>
 </template>
