e248767c382322d7a6c1bfdb7155b918273891ae
elifd <elif.dogali@adyen.com>
2026-08-26 09:42:16 +0200

    Add grant details

---
+++ packages/domains/capital/vue/src/CapitalOverview/components/GrantDetails/constants.ts
@@ -0,0 +1,5 @@
+export const GRANT_DETAILS_CLASS_NAMES = {
+    base: 'adyen-pe-grant-details',
+    content: 'adyen-pe-grant-details__content',
+    header: 'adyen-pe-grant-details__header',
+} as const;
